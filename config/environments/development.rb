8ff427d1dbcb48b7d28392fe8325b6fbb22f3f71
Bob Burbach <bob.burbach@gmail.com>
2012-03-16 15:29:20 -0700

    Move IE stylesheet and update how we include the ie stylesheet as well as ensure that it gets pre-compiled.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ MyFr2::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.assets.precompile += %w( application-ie.css )
 
   # Expands the lines which load the assets
   config.assets.debug = true
