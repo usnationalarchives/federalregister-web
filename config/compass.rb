1d8f04a25d04fb80f36c685a826d8f61880a5704
Bob Burbach <bob.burbach@gmail.com>
2012-03-19 16:16:49 -0700

    Finish setting up the compass spriting with a monkey patch for mounting the app at "/my"

--- config/compass.rb
+++ config/compass.rb
@@ -1,8 +1,4 @@
-http_path = "/my"
-http_stylesheets_path = "my/assets/"
-#http_images_dir = "assets/images"
-#http_images_path = "http://www.fr2.local/my/assets/images/"
-#http_generated_images_path = "assets/"
 relative_assets = false
 sprite_load_path = [File.expand_path( File.join( File.dirname(__FILE__), "..", "app", "assets", "images/" )), 
                     File.expand_path( File.join( File.dirname(__FILE__), "..", "vendor", "assets", "images/" ))]
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,6 @@
+# See comment at the top of config/initializers/monkey_patch.rb
+ENV['RAILS_RELATIVE_URL_ROOT'] = '/my'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
