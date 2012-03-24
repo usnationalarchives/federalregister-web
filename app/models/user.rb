fc36d3e37a2101222aefd0a7b59f1aaac74c8283
Bob Burbach <bob.burbach@gmail.com>
2012-03-24 00:37:45 -0700

    Style sign in, sign up, and password reset forms with error states

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable
+#         :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
