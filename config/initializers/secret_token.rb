426b25d95e5a063539a2fb68c723f6ced398a569
Andrew Carpenter <andrew@criticaljuncture.org>
2012-03-26 11:55:43 -0700

    remove session secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyFr2::Application.config.secret_token = '27ea78a334eb9990e3294802e8101a642171e23fd831aa7d62870f57c40fa03154209e04cc803678b0a1053023a0a3ebc81f0242596624c2d0adcea97894062b'
+MyFr2::Application.config.secret_token = SECRETS['session_secret_token'] 
