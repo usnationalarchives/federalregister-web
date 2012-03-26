0162104cde77eefa0d95cb4681d249f2e798b002
Bob Burbach <bob.burbach@gmail.com>
2012-03-26 13:02:35 -0700

    Relative root test.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # See comment at the top of config/initializers/monkey_patch.rb
-ENV['RAILS_RELATIVE_URL_ROOT'] = '/my'
+#ENV['RAILS_RELATIVE_URL_ROOT'] = '/my'
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
