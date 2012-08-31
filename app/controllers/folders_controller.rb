df2fa1037172f76c5d7e6157f15a5d51887e446f
Bob Burbach <bob.burbach@gmail.com>
2012-08-30 18:08:10 -0700

    Implement better error pages for myFR similar to how we do them in FR.

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -43,6 +43,8 @@ class FoldersController < ActionController::Base
   def show
     @folders   = FolderDecorator.decorate( Folder.scoped(:conditions => {:creator_id => current_user}).all )
     @folder    = Folder.find_by_user_and_slug(current_user, params[:id])
+    raise ActiveRecord::RecordNotFound unless @folder.present?
+
     @clippings = @folder.clippings.present? ? ClippingDecorator.decorate(@folder.clippings) : []
     
     render 'clippings/index', :layout => "application"
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module MyFr2
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Use routes to pickup exceptions - allows us to serve pretty error pages
+    config.exceptions_app = self.routes
   end
 end
