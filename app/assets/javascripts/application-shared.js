ce7f9bac29ee8881afaf261038a631b83f9f2796
Bob Burbach <bob.burbach@gmail.com>
2012-10-09 10:27:30 -0700

    Make comment field automatically expand

--- app/assets/javascripts/application-shared.js
+++ app/assets/javascripts/application-shared.js
@@ -2,6 +2,7 @@
 //= require handlebars_helpers
 //= require storage.js
 //= require analytics.js
+//= require jquery.textarea-expander.js
 //= require jquery.ui.widget.js
 //= require jquery.iframe-transport.js
 //= require jquery.fileupload.js
