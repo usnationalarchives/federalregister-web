db625d9bc42238ede9994695df3fb81033cb2cc5
Bob Burbach <bob.burbach@gmail.com>
2012-10-12 15:43:40 -0700

    Refactor js. Make validate fields a slight bit more intelligent, but only a slight bit :)

--- app/assets/javascripts/form_utils.js
+++ app/assets/javascripts/form_utils.js
@@ -1,20 +1,33 @@
+function chars_remaining(input) {
+  var $input = $(input),
+      $li = $input.closest('li'),
+      max_char = $li.data('max-size');
+
+  return max_char - $input.val().length;
+}
+
+function add_error(input) {
+  var  $input = $(input),
+       $li = $input.closest('li'),
+       warn_threshold = $li.data('size-warn-at');
+
+  return chars_remaining(input) <= warn_threshold;
+}
+
 function update_character_count(input) {
   var $input = $(input),
       $li = $input.closest('li'),
-      max_char = $li.data('max-size'),
-      warn_char = $li.data('size-warn-at'),
       error_field = $input.siblings('p.inline-errors').first();
-      console.log(max_char);
 
   if( error_field.length === 0 ) {
     $input.after( $('<p>').addClass('inline-errors') );
     error_field = $input.siblings('p.inline-errors').first();
   }
 
-  var remaining = max_char - $input.val().length,
+  var remaining = chars_remaining($input),
       text = remaining == 1 ? ' character left' : ' characters left';
 
-  if( remaining <= warn_char ) {
+  if( add_error($input) ) {
     error_field.text( remaining + text );
   } else { 
     error_field.text('');
@@ -24,11 +37,9 @@ function update_character_count(input) {
 function visually_notify_user(input) {
   var $input = $(input),
       $li = $input.closest('li'),
-      max_char = $li.data('max-size'),
-      remaining = max_char - $input.val().length,
+      remaining = chars_remaining($input),
       error_field = $input.siblings('p.inline-errors').first();
 
-  console.log(remaining, error_field);
   if( remaining < 0 ) {
     $li.addClass('error');
   } else {
@@ -53,6 +64,8 @@ function validate_field(el) {
   var $el = $(el),
       $input = $el.find('input');
 
-  $el.removeClass('error');
-  $el.find('p.inline-errors').text('');
+  if( !add_error($input) && $input.val().length > 0 ) {
+    $el.removeClass('error');
+    $el.find('p.inline-errors').text('');
+  }
 }
