1f7d77d6753616e2c427295d0505315a10e516b6
Andrew Carpenter <andrew@criticaljuncture.org>
2012-10-29 13:48:56 -0700

    add missing files

---
+++ lib/encryption_utils.rb
@@ -0,0 +1,43 @@
+module EncryptionUtils
+  def salt
+    self.salt = (self[:salt] || SecureRandom.hex(127))
+  end
+
+  def iv
+    self.iv = (self[:iv] || SecureRandom.hex(127))
+  end
+
+  def generate_cipher
+    OpenSSL::Cipher.new('aes-256-cbc')
+  end
+
+  def encryption_key
+    PBKDF2.new(:password => secret, :salt=> salt, :iterations=>1000, :key_length => 256).bin_string
+  end
+
+  def encryption_cipher
+    cipher = generate_cipher
+    cipher.encrypt
+    cipher.key = encryption_key
+    cipher.iv  = iv
+    cipher
+  end
+
+  def decryption_cipher
+    cipher = generate_cipher
+    cipher.decrypt
+    cipher.key = encryption_key
+    cipher.iv = iv
+    cipher
+  end
+
+  def encrypt(data)
+    cipher = encryption_cipher
+    cipher.update(data) + cipher.final
+  end
+  
+  def decrypt(data)
+    cipher = decryption_cipher
+    cipher.update(data) + cipher.final
+  end
+end
