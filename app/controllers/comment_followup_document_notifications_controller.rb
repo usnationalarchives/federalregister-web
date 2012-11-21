37dfcb262761902aefc4b03c7049706c9f6364e0
Bob Burbach <bob.burbach@gmail.com>
2012-11-21 12:38:43 -0800

    Fix document notifications so that they actually work as expected.

--- app/controllers/comment_followup_document_notifications_controller.rb
+++ app/controllers/comment_followup_document_notifications_controller.rb
@@ -1,8 +1,8 @@
 class CommentFollowupDocumentNotificationsController < ApplicationController
   def create
-    @comment = current_user.comments.find(:conditions => {:comment_tracking_number => params[:comment_tracking_number]})
-    @comment.followup_document_notification = false
-    @comment.save
+    @comment = current_user.comments.first(:conditions => {:comment_tracking_number => params[:comment_tracking_number]})
+    @comment.followup_document_notification = true
+    @comment.save :validate => false
 
     respond_to do |format|
       format.json { render :json => { :link_text => t('notifications.links.remove'), 
@@ -11,10 +11,10 @@ class CommentFollowupDocumentNotificationsController < ApplicationController
     end
   end
 
-  def delete
-    @comment = current_user.comments.find(:conditions => {:comment_tracking_number => params[:comment_tracking_number]})
-    @comment.followup_document_notification = true
-    @comment.save
+  def destroy
+    @comment = current_user.comments.first(:conditions => {:comment_tracking_number => params[:comment_tracking_number]})
+    @comment.followup_document_notification = false
+    @comment.save :validate => false
 
     respond_to do |format|
       format.json { render :json => { :link_text => t('notifications.links.add'), 
