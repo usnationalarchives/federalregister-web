65ecce8c0372166bd5b82972aedc7f47d2a53d49
Bob Burbach <bob.burbach@gmail.com>
2012-11-30 16:14:31 -0800

    Add confirmable to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 #         :omniauthable
 
   # Setup accessible (or protected) attributes for your model
