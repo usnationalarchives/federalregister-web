7bfdd757faaafd6425b4dabb0c0abdfa50d07047
Bob Burbach <bob.burbach@gmail.com>
2012-12-05 12:09:28 -0800

    Update signed up message to mention email confirmation. Add password reset instructions email.

--- app/mailers/fr_mailer.rb
+++ app/mailers/fr_mailer.rb
@@ -5,6 +5,7 @@ class FRMailer < Devise::Mailer
   default :from => "My Federal Register <noreply@mail.federalregister.gov>"
 
   sendgrid_enable :opentracking, :clicktracking, :ganalytics
+
   def confirmation_instructions(user)
     @user = user
     @utility_links = [['manage my subscriptions', subscriptions_url()]]
@@ -24,12 +25,38 @@ class FRMailer < Devise::Mailer
     end
   end
 
+  def reset_password_instructions(user)
+    @user = user
+    @utility_links = []
+    @highlights = EmailHighlight.pick(2)
+
+    sendgrid_category "MyFR Reset Password Instructions"
+    sendgrid_ganalytics_options :utm_source => 'federalregister.gov', :utm_medium => 'email', :utm_campaign => 'MyFR reset password'
+
+    mail(
+      :to => user.email,
+      :subject => "[FR] MyFR Reset Password Instructions"
+    ) do |format|
+      format.text { render :text => '' }
+      format.html { Premailer.new( render('reset_password_instructions'),
+                                   :with_html_string => true,
+                                   :warn_level => Premailer::Warnings::SAFE).to_inline_css }
+    end
+  end
+
+
   class Preview < MailView
     def confirmation_instructions
       user = User.new(:email => 'name@example.com',
-                      :name  => 'Jill Smith',
-                      :cofirmation => '123456789')
+                      :confirmation_token => '123456789')
       FRMailer.confirmation_instructions(user)
     end
+
+    def reset_password_instructions
+      user = User.new(:email => 'name@example.com',
+                      :reset_password_token => '123456789')
+      FRMailer.reset_password_instructions(user)
+    end
+
   end
 end
