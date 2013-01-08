2c63c4cf5cd16d8a9bcd295c50def2d9c3437745
Peregrinator <bob.burbach@gmail.com>
2013-01-08 13:58:21 -0800

    Revert "Add new handlebars helpers for formatting date (using js strftime implementation), checking if date is in past, and truncating a string to a set number of words."
    This reverts commit c856cef49f9aff326ea2c20d9df18a99e24f7282.

--- app/assets/javascripts/application-shared.js
+++ app/assets/javascripts/application-shared.js
@@ -1,5 +1,4 @@
 //= require underscore-1.2.4.min
-//= require strftime
 //= require handlebars_helpers
 //= require storage.js
 //= require analytics.js
--- app/assets/javascripts/handlebars_helpers.js
+++ app/assets/javascripts/handlebars_helpers.js
@@ -13,19 +13,3 @@ Handlebars.registerHelper('ifInFolder', function(current_document_number, block)
     return block.inverse(this);
   }
 });
-
-Handlebars.registerHelper('truncate_words', function(text, word_count) {
-  return new Handlebars.SafeString( text.split(" ").splice(0,word_count).join(" ") + "..." );
-});
-
-Handlebars.registerHelper('date_in_past', function(date, block) {
-  if( new Date(date) > new Date() ) {
-    return block(this);
-  } else {
-    return block.inverse(this);
-  }
-});
-
-Handlebars.registerHelper('formatted_date', function(date) {
-  return strftime('%m/%d/%Y', new Date(date));
-});
