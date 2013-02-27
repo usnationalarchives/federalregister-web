cb9f39b1b5d76d16af23921863235b86e0c31d1d
Peregrinator <bob.burbach@gmail.com>
2013-02-26 16:53:26 -0800

    Serve an application-shared css and javascript file that can be included before all other things. This is where common component css and js will be kept. Move jquery and handlebars to myFR so they can be included in this file.

--- app/assets/javascripts/application-shared.js
+++ app/assets/javascripts/application-shared.js
@@ -1,5 +1,7 @@
+//= require jquery-1.8.3.min
 //= require underscore-1.2.4.min
 //= require strftime
+//= require handlebars-1.0.0.rc.3
 //= require handlebars_helpers
 //= require storage.js
 //= require analytics.js
