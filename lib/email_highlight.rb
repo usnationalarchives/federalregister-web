18f5eadcdf8d60b539b7a58f213289ac78b0ef4f
Peregrinator <bob.burbach@gmail.com>
2013-07-25 11:44:25 -0700

    Refactor and use the new enabled flag

--- lib/email_highlight.rb
+++ lib/email_highlight.rb
@@ -4,8 +4,8 @@ class EmailHighlight
   HIGHLIGHTS = YAML::load_file(Rails.root.join('data', 'email_highlights.yml'))
 
   def self.weight_hash
-    @weight ||= HIGHLIGHTS.inject( Hash.new{|h,k| h[k]=[] } ) do |h, highlight|
-                  h[highlight['id']] = highlight['priority']
+    @weight ||= highlights.inject( Hash.new{|h,k| h[k]=[] } ) do |h, highlight|
+                  h[highlight.id] = highlight.priority
                   h
                 end
   end
@@ -14,6 +14,21 @@ class EmailHighlight
     pickup = Pickup.new(weight_hash, :uniq => true)
     picked = pickup.pick(num)
 
-    highlights = HIGHLIGHTS.select{|h| picked.include?(h['id'])}
+    highlights.select{|h| picked.include?(h.id)}
+  end
+
+  def self.highlights
+    @highlights ||= HIGHLIGHTS.map do |h|
+      next unless h['enabled'] == true
+
+      highlight =  OpenStruct.new(:id       => h['id'],
+                              :created  => h['created'],
+                              :title    => h['title'],
+                              :image    => h['image'],
+                              :content  => h['content'],
+                              :priority => h['priority'],
+                              :enabled  => h['enabled'])
+      highlight
+    end.compact
   end
 end
