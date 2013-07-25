0f61fb0c23a9a0cf2135355e1507a23311225cb4
Peregrinator <bob.burbach@gmail.com>
2013-07-25 15:44:46 -0700

    Use entries_search_feed_url instead of entries_search_url. Remove google and yahoo feeder links. Fix indentation.

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -2,4 +2,8 @@ module RoutesHelper
   def entries_search_path(params)
     "/articles/search?#{params.to_query}"
   end
+
+  def entries_search_feed_url(params)
+    "https://www.federalregister.gov/articles/search.rss?#{params.to_query}"
+  end
 end
