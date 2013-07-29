9fa31ee7596373a4ec63459e7360d2bdc1d54edf
Andrew Carpenter <andrew@criticaljuncture.org>
2013-07-29 15:19:16 -0700

    fix weird dependency loading issue on CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,3 +1,6 @@
+# need to explicitly load this file for CI, for unknown reasons
+Rails.root.join("spec/support/controller_macros.rb")
+
 RSpec.configure do |config|
   # devise utility method helpers
   config.include Devise::TestHelpers, :type => :controller
