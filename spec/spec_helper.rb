1900e912f34bd39b0ddd537cfa6b37ce9a9f6527
Peregrinator <bob.burbach@gmail.com>
2013-08-09 12:40:15 -0700

    remove cruft

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,13 +16,6 @@ Spork.prefork do
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
     config.mock_with :rspec
     config.use_transactional_fixtures = true
     config.use_instantiated_fixtures  = false
