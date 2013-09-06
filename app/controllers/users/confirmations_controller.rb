0bc368b1c9f0a2b0ec6e759ab27c669460ecda5e
Peregrinator <bob.burbach@gmail.com>
2013-09-05 17:13:18 -0700

    Mark all unconfirmed subscriptions confirmed when a user confirms their email address.

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,6 +5,20 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     redirect_to subscriptions_path, :notice => "Resent confirmation instructions to #{current_user.email}"
   end
 
+  # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_navigational_format?
+      sign_in(resource_name, resource)
+      current_user.subscriptions.unconfirmed.each{|s| s.confirm!}
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
+    end
+  end
+
   protected
 
   def after_confirmation_path_for(resource_name, resource)
