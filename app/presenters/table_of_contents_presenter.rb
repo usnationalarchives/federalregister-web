737f5696e28ae0b65ba32c57b482d29a06f6cae2
Peregrinator <bob.burbach@gmail.com>
2013-11-01 15:52:01 -0700

    Update subscription mailers to include documents with unrecognized agencies.

--- app/presenters/table_of_contents_presenter.rb
+++ app/presenters/table_of_contents_presenter.rb
@@ -1,7 +1,7 @@
 class TableOfContentsPresenter
   class AgencyPresenter
     attr_reader :agency, :articles, :article_count
-    delegate :id, :parent_id, :name, :url, :to => :agency
+    delegate :id, :parent_id, :url, :to => :agency
 
     def initialize(toc_view, agency)
       @toc_view = toc_view
@@ -11,6 +11,10 @@ class TableOfContentsPresenter
 
     def add_article(article)
       @articles << article
+    end
+
+    def name
+      agency.name
     end
 
     def slug
@@ -33,28 +37,47 @@ class TableOfContentsPresenter
         [type, articles_by_toc_subject]
       end
     end
-    
   end
 
-  attr_accessor :articles_without_agencies, :agencies, :agency_ids, :articles_with_agencies, :articles
+  class StubAgencyPresenter < AgencyPresenter
+    def name
+      agency.attributes["raw_name"]
+    end
+
+    def slug
+      name.downcase.gsub(/\W+/,'-')
+    end
+
+    def children
+      []
+    end
+  end
+
+  attr_accessor :agencies, :agency_ids, :articles_with_agencies, :articles
   def initialize(articles, options = {})
     @articles = ArticleDecorator.decorate(articles)
-    @articles_without_agencies, @articles_with_agencies =  @articles.sort_by{|e| [e.try_if_exists(:start_page) || 0, e.try_if_exists(:end_page) || 0, e.document_number]}.partition{|e| e.agencies.all?{|a| a.id.blank? } }
+    @articles_with_agencies =  @articles.sort_by{|e| [e.try_if_exists(:start_page) || 0, e.try_if_exists(:end_page) || 0, e.document_number]}
 
     agencies_hsh = {}
     @articles_with_agencies.each do |article|
       # create article views for all associated agencies, powering the 'See XXX'
       article.agencies.reject{|x| x.id.nil? }.each do |agency|
-        agencies_hsh[agency.id] ||= AgencyPresenter.new(self, agency)
+        agencies_hsh[agency.name] ||= AgencyPresenter.new(self, agency)
         if options[:always_include_parent_agencies] && agency.parent.present?
-          agencies_hsh[agency.parent_id] ||= AgencyPresenter.new(self, agency.parent)
+          agencies_hsh[agency.parent.name] ||= AgencyPresenter.new(self, agency.parent)
         end
       end
+
+      if article.agencies.all?{|a| a.id.nil?}
+        agency = StubAgencyPresenter.new(self, article.agencies.first)
+        agencies_hsh[agency.name] ||= agency
+        agencies_hsh[agency.name].add_article(article)
+      end      
 
       parent_agency_ids = article.agencies.map(&:parent_id).compact
       article.agencies.reject{|a| parent_agency_ids.include?(a.id)}.each do |agency|
         next if agency.id.blank?
-        agencies_hsh[agency.id].add_article(article)
+        agencies_hsh[agency.name].add_article(article)
       end
     end
     
