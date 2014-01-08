6cd9946d86b016b68dcd6eec38b1dc204b15631b
Peregrinator <bob.burbach@gmail.com>
2014-01-08 12:01:24 -0800

    Hit local api during tests

--- config/initializers/federal_register.rb
+++ config/initializers/federal_register.rb
@@ -1,5 +1,5 @@
 case Rails.env
-when 'development'
+when 'development', 'test'
   FederalRegister::Base.override_base_uri 'http://127.0.0.1:8080/api/v1'
 when 'staging'
   FederalRegister::Base.override_base_uri 'http://api.fr2.criticaljuncture.org/v1'
