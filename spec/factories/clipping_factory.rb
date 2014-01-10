e0067d14700348931a99255532268ed52d6afb05
Peregrinator <bob.burbach@gmail.com>
2014-01-09 16:33:31 -0800

    Add more documents to the test db on the FR2 side. Use these as part of the Clipping factory and in the clipping test so that the myFR and FR2 sides are communicating about the same documents via the api.

--- spec/factories/clipping_factory.rb
+++ spec/factories/clipping_factory.rb
@@ -1,6 +1,18 @@
 FactoryGirl.define do
   factory :clipping do
-    user_id 1
+    user { User.first || build(:user) }
     sequence(:document_number) {|n| "2011-#{1000+n}"}
+  end
+
+  factory :fr_clipping_1, parent: :clipping do
+    document_number "4242-4242"
+  end
+
+  factory :fr_clipping_2, parent: :clipping do
+    document_number "4343-4343"
+  end
+
+  factory :fr_clipping_3, parent: :clipping do
+    document_number "4444-4444"
   end
 end
--- spec/features/clippings_spec.rb
+++ spec/features/clippings_spec.rb
@@ -19,7 +19,7 @@ feature "Creating clippings" do
 
       # warning message and clipped document should be visible
       expect(page).to have_selector('.warning.message p', text: 'These clippings are not permanently saved. Please sign in or sign up to save them permanently.')
-      expect(page).to have_clipping_with_title('Test document')
+      expect(page).to have_clipping_with_title('Test Document')
     end
   end
 
@@ -30,20 +30,20 @@ feature "Creating clippings" do
       clip_current_document
       click_user_util_item('Sign up')
 
-      # create user
-      user = FactoryGirl.build(:user)
+      # sign up user
+      user = build(:user)
       manually_sign_up(user.email, user.password)
 
       # message document was saved and the document itself should be visible
       expect(page).to have_selector('.notice.message p', text: '1 clipping was in temporary storage. It has been saved to your clipboard.')
-      expect(page).to have_clipping_with_title('Test document')
+      expect(page).to have_clipping_with_title('Test Document')
     end
   end
 
   context "a user with an account" do
     scenario "clips a document while not signed in, signs in, it should be added to their list of documents", :js do
-      user = FactoryGirl.create(:user)
-      clipping = FactoryGirl.create(:clipping, user_id: user.id)
+      user = create(:user)
+      clipping = create(:fr_clipping_2, user_id: user.id)
       user.clippings = [clipping]
       user.save
 
@@ -57,21 +57,20 @@ feature "Creating clippings" do
 
       # message document was saved and the document itself should be visible
       expect(page).to have_selector('.notice.message p', text: '1 clipping was in temporary storage. It has been saved to your clipboard.')
-      expect(page).to have_clipping_with_title('Test document')
+      expect(page).to have_clipping_with_title('Test Document')
 
       # two documents should be clipped
       expect(page).to have_user_util("#doc_count", 2)
       expect( page.all('#clippings li').count ).to eq(2)
     end
 
-    scenario "clips a document while signed in, count should increment, and should show in the clipboard", :js do
-      user = FactoryGirl.create(:user)
-      clipping = FactoryGirl.create(:clipping, user_id: user.id)
-      user.clippings = [clipping]
-      user.save
+    scenario "clips a document while signed in, count should increment, and should show in the clipboard", :js, :focus do
+      user = create(:user)
+      clipping = create(:fr_clipping_2, user_id: user.id)
 
       manually_sign_in(user.email, user.password)
 
+      #binding.pry
       visit '/articles/2014/01/01/4242-4242/test-document'
       expect(page).to have_user_util("#doc_count", 1)
 
@@ -82,7 +81,7 @@ feature "Creating clippings" do
       click_nav_item("My FR", "My Clipboard")
 
       # the document should be visible
-      expect(page).to have_clipping_with_title('Test document')
+      expect(page).to have_clipping_with_title('Test Document')
 
       # two documents should be clipped
       expect(page).to have_user_util("#doc_count", 2)
