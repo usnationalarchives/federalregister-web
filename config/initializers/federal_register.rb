bddb945be8d1add6d3b91fd7cd71bc3447ce39e5
Peregrinator <bob.burbach@gmail.com>
2014-01-09 16:35:41 -0800

    Test environment needs to use the api connected to the fr2 test db.

--- config/initializers/federal_register.rb
+++ config/initializers/federal_register.rb
@@ -1,6 +1,8 @@
 case Rails.env
-when 'development', 'test'
+when 'development'
   FederalRegister::Base.override_base_uri 'http://127.0.0.1:8080/api/v1'
+when 'test'
+  FederalRegister::Base.override_base_uri 'http://127.0.0.1:8081/api/v1'
 when 'staging'
   FederalRegister::Base.override_base_uri 'http://api.fr2.criticaljuncture.org/v1'
 when 'production'
