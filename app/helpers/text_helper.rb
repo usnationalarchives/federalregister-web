2c66991a908e18bba42bff73f60126ba6b5c06d7
Peregrinator <bob.burbach@gmail.com>
2014-02-18 15:23:58 -0800

    Add roman number helper

--- app/helpers/text_helper.rb
+++ app/helpers/text_helper.rb
@@ -3,13 +3,13 @@ module TextHelper
     return if text.nil?
     length = options[:length] || 30
     omission = options[:omission] || '...'
-    
+
     if text.length <= length
       text
     else
       words = text.split(/\s+/)
       l = length - omission.length
-      
+
       words.inject('') do |str, word|
         new_str = str == '' ? word : "#{str} #{word}"
         if new_str.length > l
@@ -19,5 +19,10 @@ module TextHelper
         end
       end
     end
+  end
+
+  ROMANS = %w(undef I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX)
+  def number_to_roman(i)
+    ROMANS[i.to_i]
   end
 end
