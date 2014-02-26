45b3a40523ec7917e4b309b0d96d279f5c845f1f
Peregrinator <bob.burbach@gmail.com>
2014-02-25 16:16:39 -0800

    Implement public comment bar

--- app/decorators/document_decorator.rb
+++ app/decorators/document_decorator.rb
@@ -70,5 +70,33 @@ class DocumentDecorator < ApplicationDecorator
   def has_comments?
     regulations_dot_gov_info && regulations_dot_gov_info['comments_count'] > 0
   end
+
+  def accepting_comments?
+    document_comment_period_open? || regulations_dot_gov_accepting_comments?
+  end
+
+  def document_comment_period_open?
+    comments_close_on.present? && comments_close_on >= Time.current.to_date
+  end
+
+  # occasionally comment periods are extended on regulations.gov past the
+  # originally published date in the document
+  def regulations_dot_gov_accepting_comments?
+    comment_url.present? && publication_date.to_time > 4.months.ago
+  end
+
+  def formal_comment_link
+    link_text = "Submit a formal comment"
+
+    if comment_url.present?
+      href = comment_url
+      options = { target: '_blank', :'data-comment' => 1}
+    else
+      href = '#addresses'
+      options = {}
+    end
+
+    h.link_to link_text, href, {id: 'start_comment', class: 'button formal_comment'}.merge(options)
+  end
   end
 end
