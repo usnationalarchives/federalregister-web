a0a5358386a816e583fe5252c6d9394ccd5f5c23
Peregrinator <bob.burbach@gmail.com>
2014-02-25 16:17:52 -0800

    Implement corrections

--- app/decorators/document_decorator.rb
+++ app/decorators/document_decorator.rb
@@ -98,5 +98,28 @@ class DocumentDecorator < ApplicationDecorator
 
     h.link_to link_text, href, {id: 'start_comment', class: 'button formal_comment'}.merge(options)
   end
+
+  def corrections?
+    correction_of.present? || corrections.present?
+  end
+
+  def republication?
+    document_number =~ /^R/
+  end
+
+  def correction_of_document
+    return @doc if @doc
+
+    doc_num = correction_of.split('documents/').last.split('.').first
+    @doc = DocumentDecorator.decorate(FederalRegister::Document.find(doc_num, fields: ["html_url", "publication_date"]))
+  end
+
+  def corrected_documents
+    return @corrected_documents if @corrected_documents
+
+    @corrected_documents = corrections.map do |correction|
+      doc_num = correction.split('documents/').last.split('.').first
+      DocumentDecorator.decorate(FederalRegister::Document.find(doc_num, fields: ["document_number", "html_url", "publication_date"]))
+    end
   end
 end
