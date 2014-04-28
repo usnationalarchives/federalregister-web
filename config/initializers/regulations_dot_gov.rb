9223753d14169c7be40a5db4f838b5c5a3f9337b
Peregrinator <bob.burbach@gmail.com>
2014-04-28 15:21:46 -0700

    Update regulations_dot_gov settings

--- config/initializers/regulations_dot_gov.rb
+++ config/initializers/regulations_dot_gov.rb
@@ -1,5 +1,9 @@
 RegulationsDotGov::Client.api_key = SECRETS['data_dot_gov']['api_key'] || 'DEMO_KEY'
 
 if Rails.env.development? || Rails.env.test?
-  RegulationsDotGov::Client.override_base_uri('http://api.data.gov/TEST/regulations/v2/')
+  RegulationsDotGov::Client.override_base_uri('http://api.data.gov/TEST/regulations/v3/')
+end
+
+if Rails.env.staging? || Rails.env.production?
+  RegulationsDotGov::Client.override_base_uri('http://api.data.gov/regulations/v3/')
 end
