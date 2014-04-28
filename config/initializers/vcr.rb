3d7f553226eeb2253c7ceea97b307bb0ed37e5d9
Peregrinator <bob.burbach@gmail.com>
2014-04-28 15:21:28 -0700

    Remove old vcr recordings. Update vcr settings

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -1,11 +1,12 @@
-if defined?(VCR)
+if defined?(VCR) #&& Rails.env.test?
   VCR.configure do |c|
-    c.default_cassette_options = { :record => :once }
+    c.default_cassette_options = { :record => :new_episodes }
     c.ignore_hosts '127.0.0.1', 'localhost'
     c.cassette_library_dir = Rails.root.join("spec", "vcr")
     c.hook_into :fakeweb
 
-    c.filter_sensitive_data('DEMO_KEY') { SECRETS['data_dot_gov']['api_key'] || 'DEMO_KEY' }
+    c.filter_sensitive_data('<API_KEY>') { SECRETS['data_dot_gov']['api_key'] }
+    c.filter_sensitive_data('<DEMO_KEY>') { 'DEMO_KEY' }
     c.debug_logger = File.open(Rails.root.join("log", "vcr.log"), 'w')
   end
 
