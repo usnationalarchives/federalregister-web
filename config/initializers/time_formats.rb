d16cadeabddf39d22f4e0bd97a12921b75f274ed
Peregrinator <bob.burbach@gmail.com>
2014-04-30 11:20:07 -0700

    Add comment due date to comment form header once the form is retrieved. Update language on comment success page.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -4,5 +4,7 @@ Date::DATE_FORMATS[:formal] = lambda { |time| time.strftime("%A, %B #{time.day.o
 
 Time::DATE_FORMATS[:date] = "%m/%d/%Y"
 Time::DATE_FORMATS[:datetime] = "%m/%d/%Y at %I:%M %P"
+Time::DATE_FORMATS[:datetime_with_zone] = "%m/%d/%Y at %I:%M%P %Z"
+
 # "Friday, April 22nd, 2011 at 1:30 PM"
 Time::DATE_FORMATS[:formal_with_time] = lambda { |time| time.strftime("%A, %B #{time.day.ordinalize}, %Y at %I:%M %p") }
