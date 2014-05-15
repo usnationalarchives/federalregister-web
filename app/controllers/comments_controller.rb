eaff9954e28cf01c2158927f7c5606e3134e27be
Peregrinator <bob.burbach@gmail.com>
2014-05-15 16:53:57 -0700

    Deal with possible error paths through the comment process.
    * User submits form but we get an error from regulations.gov - first we reload the form to ensure we have an up to date from and that the agency hasn’t added a required field since we last cached it. Then we attempt to save that form. If it’s invalid we show the user the invalid form per usual with validation errors. However if it passes our validation it will be submitted again. If that second submission is also an error (unexpected form config, unplanned outage since the first submission, etc) we simply rescue that and show the user whatever message came back as there is nothing more we can do at that point.
    * Other path is that we simply can’t communicate with regulations.gov. So we let the user know and provide a link to their site where the user can go to see what is happening.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -34,14 +34,48 @@ class CommentsController < ApplicationController
     end
 
     if @comment.save
-      @comment = CommentDecorator.decorate(@comment)
-
-      CommentMailer.comment_copy(user, @comment).deliver if user_signed_in?
-
-      render :action => :show, :status => 200
+      render_created_comment
     else
-      render :action => :new, :status => 422
+      render_error_page
     end
+  rescue RegulationsDotGov::Client::InvalidSubmission => exception
+    begin
+      # reload the form from regs.gov
+      @comment.comment_form = load_comment_form(:read_from_cache => false)
+
+      record_regulations_dot_gov_error(
+        parse_message(exception.message),
+        @comment.document_id
+      )
+
+     # try to save with the updated form from the reload above
+      if @comment.save
+        render_created_comment
+      else
+        # show invalid form to user with our error messages
+        # if say a required field was added after the form was last cached by us
+        render_error_page
+      end
+    rescue RegulationsDotGov::Client::ResponseError => inner_exception
+      record_regulations_dot_gov_error(
+        parse_message(inner_exception.message),
+        @comment.document_id
+      )
+
+      # show form to user but with message from regulations.gov
+      render_error_page(
+        parse_message(inner_exception.message, 'message')
+      )
+    end
+  rescue RegulationsDotGov::Client::ResponseError => exception
+    record_regulations_dot_gov_error(
+      exception,
+      @comment.document_id
+    )
+
+    render_error_page(
+      "We had trouble communicating with Regulations.gov; try again or visit #{link_to @comment.article.comment_url, @comment.article.comment_url}"
+    )
   end
 
   def persist_for_login
@@ -58,12 +92,31 @@ class CommentsController < ApplicationController
 
   private
 
+  def render_created_comment
+    @comment = CommentDecorator.decorate(@comment)
+
+    CommentMailer.comment_copy(user, @comment).deliver if user_signed_in?
+
+    render :action => :show, :status => 200
+  end
+
+  def render_error_page(error=nil)
+    if error
+      @comment.errors.add(
+        :base,
+        error
+      )
+    end
+
+    render :action => :new, :status => 422
+  end
+
   def load_entry
     @entry = FederalRegister::Article.find(params[:document_number])
   end
 
-  def load_comment_form
-    client = RegulationsDotGov::Client.new
+  def load_comment_form(options={})
+    client = RegulationsDotGov::Client.new(options)
 
     if @entry.regulations_dot_gov_url
       document_id = @entry.regulations_dot_gov_url.split(/=/).last
@@ -78,5 +131,31 @@ class CommentsController < ApplicationController
     @comment = Comment.new(:comment_form => @comment_form, :document_number => @entry.document_number)
     @comment.attributes = params[:comment] if params[:comment]
     @comment_attachments = @comment.attachments
+  end
+
+  def record_regulations_dot_gov_error(exception, document_id)
+    Rails.logger.error(exception)
+
+    honeybadger_options = {
+      :exception => exception,
+      :parameters => {
+        :document_number => document_id
+      }
+    }
+    notify_honeybadger(honeybadger_options)
+  end
+
+  # sometimes the message is a json encoded string
+  # other times it's just a string
+  def parse_message(message, key=nil)
+    begin
+      if key
+        JSON.parse(message)[key]
+      else
+        JSON.parse(message)
+      end
+    rescue JSON::ParserError
+      message
+    end
   end
 end
