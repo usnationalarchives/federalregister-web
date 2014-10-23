8ad213943fec7e0f8ecae7e01b2cd6ed22750530
Rich Wallett <richmang@gmail.com>
2014-10-23 16:10:22 -0700

    move agency searches to presenter, update views

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -7,48 +7,12 @@ class AgenciesController < ApplicationController
   end
 
   def show
-    @presenter = AgenciesPresenter.new(FederalRegister::Agency.all)
-    @agency = @presenter.agency(params[:id])
+    @presenter = AgenciesPresenter.new(FederalRegister::Agency.all, params[:id])
+    @agency = @presenter.agency
     respond_to do |wants|
-      wants.html do
-        @public_inspection_search_results = FederalRegister::PublicInspectionDocument.search(
-          conditions: {
-            agency_ids: [@agency.id]
-          },
-          per_page: 250,
-          order: 'newest',
-        )
-
-        @public_inspection_documents = @public_inspection_search_results.
-          map{|document| DocumentDecorator.decorate(document)}
-
-        @document_search_results = FederalRegister::Article.search(
-          conditions: {
-            agency_ids: [@agency.id]
-          },
-          order: 'newest',
-          per_page: 40
-        )
-
-        @documents = @document_search_results.
-          map{|document| DocumentDecorator.decorate(document)}
-
-        @significant_documents_search_results = FederalRegister::Article.search(
-          conditions: {
-            agency_ids: [@agency.id],
-            significant: '1',
-            publication_date: {
-              gte: 3.months.ago.to_date.to_s
-            }
-          },
-          order: 'newest',
-          per_page: 40
-        )
-        @significant_documents = @significant_documents_search_results.
-          map{|document| DocumentDecorator.decorate(document)}
+      wants.html
       # @comments_closing = @agency.entries.comments_closing
       # @comments_opening = @agency.entries.comments_opening
-      end
 
       wants.rss do
         base_url = 'https://www.federalregister.gov/articles/search.rss?'
--- app/presenters/agencies_presenter.rb
+++ app/presenters/agencies_presenter.rb
@@ -1,17 +1,57 @@
 class AgenciesPresenter
-  attr_reader :agencies, :agency
-  def initialize(agencies)
+  attr_reader :agencies,
+    :agency,
+    :public_inspection_documents
+
+  def initialize(agencies, agency_slug)
     @agencies = agencies
+    @agency = get_agency(agency_slug)
   end
 
-  def agency(slug)
+  def get_agency(slug)
     AgencyDecorator.
       new(
         agencies.
-          detect{|agency| agency.url.split('/').last == slug},
+          detect{|a| a.url.split('/').last == slug},
         agencies
       )
   end
+
+  def documents
+    @documents ||= FederalRegister::Article.search(
+      conditions: {
+        agency_ids: [@agency.id]
+      },
+      order: 'newest',
+      per_page: 40
+    ).map{|document| DocumentDecorator.decorate(document)}
+  end
+
+  def significant_documents
+    @significant_documents ||= FederalRegister::Article.search(
+      conditions: {
+        agency_ids: [@agency.id],
+        significant: '1',
+        publication_date: {
+          gte: 3.months.ago.to_date.to_s
+        }
+      },
+      order: 'newest',
+      per_page: 40
+    ).map{|document| DocumentDecorator.decorate(document)}
+  end
+
+
+  def public_inspection_documents
+    @public_inspection_documents ||= FederalRegister::PublicInspectionDocument.search(
+      conditions: {
+        agency_ids: [agency.id]
+      },
+      per_page: 250,
+      order: 'newest',
+    ).map{|document| DocumentDecorator.decorate(document)}
+  end
+
 
   def parent_agencies
     agencies.
