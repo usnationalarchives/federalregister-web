0e3cdc97920c7b906c4e8a5e420d8df3e2957915
Peregrinator <bob.burbach@gmail.com>
2015-01-05 17:42:17 -0800

    Update api path for staging/officialness staging

--- config/initializers/federal_register.rb
+++ config/initializers/federal_register.rb
@@ -4,7 +4,9 @@ when 'development'
 when 'test'
   #FederalRegister::Base.override_base_uri 'http://127.0.0.1:8081/api/v1'
 when 'staging'
-  #FederalRegister::Base.override_base_uri 'http://api.fr2.criticaljuncture.org/v1'
+  FederalRegister::Base.override_base_uri 'http://api.fr2.criticaljuncture.org/v1'
+when 'officialness_staging'
+  FederalRegister::Base.override_base_uri 'http://fr-official.criticaljuncture.org/api/v1'
 when 'production'
 end
 
