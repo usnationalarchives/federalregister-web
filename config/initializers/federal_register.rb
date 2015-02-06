08b785fccaa0074ecb03414319cebd74c43078b3
Peregrinator <bob.burbach@gmail.com>
2015-02-06 13:03:36 -0800

    Use /api/v1 as url instead of subdomain

--- config/initializers/federal_register.rb
+++ config/initializers/federal_register.rb
@@ -2,7 +2,7 @@ case Rails.env
 when 'development'
   FederalRegister::Base.override_base_uri 'http://127.0.0.1:8080/api/v1'
 when 'staging'
-  FederalRegister::Base.override_base_uri 'http://api.fr2.criticaljuncture.org/v1'
+  FederalRegister::Base.override_base_uri 'https://fr2.criticaljuncture.org/api/v1'
 when 'production'
 end
 
