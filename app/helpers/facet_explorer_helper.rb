fa5e74e35409fb4460cca272e4470b4f687dc3b0
Peregrinator <bob.burbach@gmail.com>
2015-03-11 12:35:27 -0700

    Add facet_explorer_helper and update homepage and navigation topics/agencies to all use this helper

---
+++ app/helpers/facet_explorer_helper.rb
@@ -0,0 +1,36 @@
+module FacetExplorerHelper
+  def facet_explorer(args={})
+    content_tag :div, class: 'facet-explorer' do
+      facet_explorer_header( args.fetch(:header) ) +
+      facet_explorer_search( args.fetch(:search) ) +
+      facet_explorer_table( args.fetch(:table) )
+    end
+  end
+
+  def facet_explorer_header(args)
+    render partial: 'facets/explorer/header',
+      locals: {
+        icon: args.fetch(:icon),
+        title: args.fetch(:title),
+      }
+  end
+
+  def facet_explorer_search(args)
+    render partial: 'facets/explorer/search',
+      locals: {
+        url: args.fetch(:url),
+        placeholder: args.fetch(:placeholder),
+      }
+  end
+
+  def facet_explorer_table(args)
+    render partial: 'facets/explorer/table',
+      locals: {
+        items: args.fetch(:items),
+        header_count_width: args.fetch(:header_count_width),
+        utm_source: args.fetch(:utm_source),
+        utm_medium: args.fetch(:utm_medium),
+        utm_content_type: args.fetch(:utm_content_type),
+      }
+  end
+end
