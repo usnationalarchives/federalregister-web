f210381be41fce9d4f5589667053df13a0508e31
Brandon Carag <brandon.carag@gmail.com>
2015-03-11 10:59:06 -0700

    Updated metadata bar view logic, built out public inspection presenter, and changed instance variables in the special_controller for #current_issue.

--- app/controllers/special_controller.rb
+++ app/controllers/special_controller.rb
@@ -32,8 +32,8 @@ class SpecialController < ApplicationController
   end
 
   def current_issue
-    @doc_presenter = DocumentIssuePresenter.new(Date.current - 5.days)
-    @pi_presenter = HomepagePublicInspectionIssuePresenter.new(Date.current)
+    @doc_presenter = DocumentIssuePresenter.new(Date.current - 2.days)
+    @pi_presenter = HomepagePublicInspectionIssuePresenter.new(Date.current - 2.days)
     render partial: 'metadata_bar'
   end
 
--- app/presenters/homepage_public_inspection_issue_presenter.rb
+++ app/presenters/homepage_public_inspection_issue_presenter.rb
@@ -1,4 +1,10 @@
 class HomepagePublicInspectionIssuePresenter
+  FILING_DISPLAY_INFO = { 
+    "NOTICE" => {:icon => "notice", :tooltip => "Notices"},
+    "PRORULE" => {:icon => "proposed_rule", :tooltip => "Proposed Rules" },
+    "RULE" => {:icon => "rule" , :tooltip => "Rules" }
+  }
+
   attr_reader :date
 
   def initialize(date)
@@ -8,15 +14,28 @@ class HomepagePublicInspectionIssuePresenter
   def regular_filing_counts
     hsh = Hash.new(0)
     get_regular_filing_counts.map do |filing|
-      RegularFiling.new(
+      Filing.new(
         count: filing.count,
         search_conditions: filing.result_set.conditions,
         slug: filing.slug
       )
-    end.each {|regular_filing| hsh[regular_filing.slug] = regular_filing }
+    end.each {|filing| hsh[filing.slug] = filing }
     hsh
   end
-    class RegularFiling
+
+  def special_filing_counts
+    hsh = Hash.new(0)
+    get_special_filing_counts.map do |filing|
+      Filing.new(
+        count: filing.count,
+        search_conditions: filing.result_set.conditions,
+        slug: filing.slug
+      )
+    end.each {|filing| hsh[filing.slug] = filing }
+    hsh
+  end
+
+    class Filing
       vattr_initialize [
         :count,
         :search_conditions,
@@ -25,18 +44,32 @@ class HomepagePublicInspectionIssuePresenter
     end
 
   def regular_filing_count
-    if get_regular_filing_counts.first
+    if get_regular_filing_counts.first # Guarding against empty hash
       search_conditions = get_regular_filing_counts.first.result_set.conditions 
     else
       search_conditions = nil
     end
-    TotalCountRegularFiling.new(
+    TotalFiling.new(
       total_count: get_regular_filing_counts.inject(0){|total_sum, filing| total_sum + filing.count },
       sample_filing: get_regular_filing_counts.try(:first),
-      search_conditions: search_conditions #get_regular_filing_counts.first.result_set.conditions
+      search_conditions: search_conditions
     )
   end
-    class TotalCountRegularFiling
+
+  def special_filing_count
+    if get_special_filing_counts.first # Guarding against empty hash
+      search_conditions = get_special_filing_counts.first.result_set.conditions 
+    else
+      search_conditions = nil
+    end
+    TotalFiling.new(
+      total_count: get_special_filing_counts.inject(0){|total_sum, filing| total_sum + filing.count },
+      sample_filing: get_special_filing_counts.try(:first),
+      search_conditions: search_conditions
+    )
+  end
+
+    class TotalFiling
       vattr_initialize [
         :total_count,
         :sample_filing,
@@ -50,4 +83,8 @@ class HomepagePublicInspectionIssuePresenter
       search(:conditions => {:special_filing => 0, :filed_at => {:is => date }})
   end
 
+  def get_special_filing_counts
+    @filings = FederalRegister::Facet::PublicInspectionDocument::Type.
+      search(:conditions => {:special_filing => 1, :filed_at => {:is => date }})
+  end
 end
