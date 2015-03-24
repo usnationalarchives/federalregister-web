03d66d149f7267cf853230a7eb4feee06bf70aae
Brandon Carag <brandon.carag@gmail.com>
2015-03-24 13:40:14 -0700

    In accounts, replace show action with index action.

--- app/controllers/users/accounts_controller.rb
+++ app/controllers/users/accounts_controller.rb
@@ -4,20 +4,16 @@ def index
   @user = User.find(current_user.id)
 end
 
-def show
-  @user = User.find(current_user.id)
-end
-
 def update
   user = User.find(current_user.id)
   if user.update_attributes(strong_params.merge(confirmed_at: nil))
     user.send_confirmation_instructions
-    user.subscriptions.each {|sub|sub.confirmed_at = nil; sub.save}
+    user.subscriptions.each {|s|s.confirmed_at = nil; s.save}
     flash[:notice] = "A confirmation email has been sent to #{params[:email]}"
     redirect_to accounts_path
   else
     flash[:error] = "Something went wrong with your update request.  Please re-enter your email address and try again."
-    render 'show'
+    render 'index'
   end
 end
 
