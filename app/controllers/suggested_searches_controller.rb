5ebf26a903f43b3baa149085db773aeb9953312b
Brandon Carag <brandon.carag@gmail.com>
2015-03-27 10:41:47 -0700

    Refactored document_type icon helper, added error handling for section/suggested page controllers and presenters, created suggested searches partial, and modified suggested_searches#show template.

--- app/controllers/suggested_searches_controller.rb
+++ app/controllers/suggested_searches_controller.rb
@@ -2,7 +2,12 @@ class SuggestedSearchesController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def show
+    begin
     @presenter = SuggestedSearchPresenter.new(SuggestedSearch.search(conditions: {sections: ['money']})['money'].first)
+    #TODO: Add API Endpoint search instead of stub.
+    rescue SuggestedSearchPresenter.InvalidSuggestedSearch
+      raise ActiveRecord::RecordNotFound
+    end
   end
 
 end
