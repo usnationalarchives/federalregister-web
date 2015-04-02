bb7ced45056f58af91abecff143e1386b2f81ec1
Peregrinator <bob.burbach@gmail.com>
2015-04-01 18:30:31 -0700

    Move public inspection to public inspection documents to match the controller

--- app/controllers/public_inspection_documents_controller.rb
+++ app/controllers/public_inspection_documents_controller.rb
@@ -4,19 +4,16 @@ class PublicInspectionDocumentsController < ApplicationController
 
   def index
     cache_for 1.day
-
-    render
   end
 
   def current
     cache_for 1.day
     @documents = []
-
-    render template: 'public_inspection/index'
   end
 
   def public_inspection
-    redirect_to current_public_inspection_documents_path, status: :moved_permanently
+    redirect_to current_public_inspection_documents_path,
+      status: :moved_permanently
   end
 
   def navigation
