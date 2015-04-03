aa3f932ba52422fabbd7c67192931b23e33ff467
Peregrinator <bob.burbach@gmail.com>
2015-04-03 15:08:41 -0700

    Fix devise deprecation warning

--- config/routes.rb
+++ config/routes.rb
@@ -292,7 +292,8 @@ MyFr2::Application.routes.draw do
     devise_for :users, :controllers => { :passwords => "users/passwords",
                                          :confirmations => "users/confirmations",
                                          :sessions => "users/sessions",
-                                         :registrations => "users/registrations" } do
+                                         :registrations => "users/registrations" }
+    devise_scope :user do
       get 'sign_in', :to => 'users/sessions#new', :as => :new_session
       get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_session
       get 'sign_up', :to => 'users/registrations#new', :as => :user_registration
