aaab5e6c1604d1f446f67ed9f0fac70aa081b853
Peregrinator <bob.burbach@gmail.com>
2015-04-03 18:44:26 -0700

    Support <E> with attributes of T=02, T=03, T=34

--- app/views/xslt/matchers/typeface.html.xslt
+++ app/views/xslt/matchers/typeface.html.xslt
@@ -1,10 +1,15 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-  <!--
-    text italicised in header
-    See page 9 here: http://www.gpo.gov/fdsys/pkg/FR-2015-01-22/pdf/2015-00344.pdf
-  -->
-  <xsl:template match="E[@T=7462]">
+
+  <xsl:template match="E[@T=02]">
+    <xsl:call-template name="optional_preceding_whitespace" />
+    <strong class="small-caps">
+      <xsl:apply-templates />
+    </strong>
+    <xsl:call-template name="optional_following_whitespace" />
+  </xsl:template>
+
+  <xsl:template match="E[@T=03]">
     <xsl:call-template name="optional_preceding_whitespace" />
     <em>
       <xsl:apply-templates />
@@ -20,5 +25,24 @@
     <xsl:call-template name="optional_following_whitespace" />
   </xsl:template>
 
+  <xsl:template match="E[@T=34]">
+    <xsl:call-template name="optional_preceding_whitespace" />
+    <em class="small-caps">
+      <xsl:apply-templates />
+    </em>
+    <xsl:call-template name="optional_following_whitespace" />
+  </xsl:template>
+
+  <!--
+    text italicised in header
+    See page 9 here: http://www.gpo.gov/fdsys/pkg/FR-2015-01-22/pdf/2015-00344.pdf
+  -->
+  <xsl:template match="E[@T=7462]">
+    <xsl:call-template name="optional_preceding_whitespace" />
+    <em>
+      <xsl:apply-templates />
+    </em>
+    <xsl:call-template name="optional_following_whitespace" />
+  </xsl:template>
 
 </xsl:stylesheet>
--- spec/xslt/full_text/typeface.rb
+++ spec/xslt/full_text/typeface.rb
@@ -1,22 +1,74 @@
 require './spec/support/xslt_test_helper'
 include XsltTestHelper
 
-describe "XSLT::FullText::Headers" do
+describe "XSLT::FullText::Typeface" do
   before :all do
     @template = "matchers/full_text.html.xslt"
   end
 
-  it "formats T=04 as strong" do
-    process <<-XML
-      <P>
-        In a final rule published in the<E T="04">Federal Register</E>on November 10
-      </P>
-    XML
+  context "<E> node attributes" do
+    it "formats T=02 as strong with small-caps class" do
+      process <<-XML
+        <P>
+          The street address for the Docket Operations office is in the
+          <E T="02">ADDRESSES</E>
+          section.
+        </P>
+      XML
 
-    expect_equivalent <<-HTML
-      <p id="p-1" data-page="1000">
-        In a final rule published in the <strong>Federal Register</strong> on November 10
-      </p>
-    HTML
+      expect_equivalent <<-HTML
+        <p id="p-1" data-page="1000">
+          The street address for the Docket Operations office is in the <strong class="small-caps">ADDRESSES</strong> section.
+        </p>
+      HTML
+    end
+
+    it "formats T=03 as emphasis" do
+      process <<-XML
+        <P>
+          For service information identified in this AD, contact Airbus SAS email
+          <E T="03">airworthiness.A330@airbus.com;</E>
+          Internet
+          <E T="03">http://www.airbus.com.</E>
+          You may view this referenced service...
+        </P>
+      XML
+
+      expect_equivalent <<-HTML
+        <p id="p-1" data-page="1000">
+          For service information identified in this AD, contact Airbus SAS email <em>airworthiness.A330@airbus.com;</em> Internet <em>http://www.airbus.com.</em> You may view this referenced service...
+        </p>
+      HTML
+    end
+
+    it "formats T=04 as strong" do
+      process <<-XML
+        <P>
+          In a final rule published in the<E T="04">Federal Register</E>on November 10
+        </P>
+      XML
+
+      expect_equivalent <<-HTML
+        <p id="p-1" data-page="1000">
+          In a final rule published in the <strong>Federal Register</strong> on November 10
+        </p>
+      HTML
+    end
+
+    it "formats T=34 as emphasis" do
+      process <<-XML
+        <P>
+          applicability determination before use of the minor NSR tools.
+          <E T="34">Alaska Admin. Code</E>
+          tit. 18, 50.502, approved 72 FR 45378 (August 14, 2007); 7
+        </P>
+      XML
+
+      expect_equivalent <<-HTML
+        <p id="p-1" data-page="1000">
+          applicability determination before use of the minor NSR tools. <em class="small-caps">Alaska Admin. Code</em> tit. 18, 50.502, approved 72 FR 45378 (August 14, 2007); 7
+        </p>
+      HTML
+    end
   end
 end
