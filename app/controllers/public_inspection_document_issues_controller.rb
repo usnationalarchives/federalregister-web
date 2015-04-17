ff0e924f525089a121be86d5e512f1e370d27ecb
Brandon Carag <brandon.carag@gmail.com>
2015-04-17 14:15:17 -0700

    Fixed CSS issue with issue summary bar, ensured “updated at” information in public inspection TOC are dynamic.

--- app/controllers/public_inspection_document_issues_controller.rb
+++ app/controllers/public_inspection_document_issues_controller.rb
@@ -5,6 +5,7 @@ class PublicInspectionDocumentIssuesController < ApplicationController
     if PublicInspectionDocumentIssue.available_on(parse_date_from_params)
       raise ActiveRecord::RecordNotFound
     else
+      @pi_presenter = PublicInspectionIssuePresenter.new(parse_date_from_params)
       @special_filings_presenter = TableOfContentsSpecialFilingsPresenter.new(parse_date_from_params)
       @regular_filings_presenter = TableOfContentsRegularFilingsPresenter.new(parse_date_from_params)
     end
