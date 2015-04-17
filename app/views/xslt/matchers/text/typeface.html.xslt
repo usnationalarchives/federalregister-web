567048b26215d5e01bb3352586b317b474d25a2b
Peregrinator <bob.burbach@gmail.com>
2015-04-16 17:09:00 -0700

    Implement superscripts, subscripts and italic versions of each with proper whitespace between text and punctuation

--- app/views/xslt/matchers/text/typeface.html.xslt
+++ app/views/xslt/matchers/text/typeface.html.xslt
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
   <xsl:include href="../../templates/formatting.html.xslt" />
+  <xsl:include href="../../templates/whitespace.html.xslt" />
 
   <xsl:template match="E[@T=02]">
     <xsl:call-template name="optional_preceding_whitespace" />
@@ -34,16 +35,88 @@
     <xsl:call-template name="optional_following_whitespace" />
   </xsl:template>
 
+
+
+  <!--  trim the trailing whitespace from the text element directly
+        preceeding a subscript so that the subscript is positioned correctly -->
+  <xsl:template match="text()[
+      following-sibling::*[1][self::E][@T=51 or @T=52 or @T=53 or @T=54]
+    ]">
+    <xsl:call-template name="string-rtrim">
+      <xsl:with-param name="string" select="." />
+    </xsl:call-template>
+  </xsl:template>
+
+  <!--  trim the preceding whitespace from the text element directly following
+        a subscript if it starts with a ', | ; | : | ) | ] so that the text
+        is position correctly -->
+  <xsl:template match="text()[
+      preceding-sibling::*[1][self::E][@T=51 or @T=52 or @T=53 or @T=54]
+      and
+      contains(',;:)]', substring(normalize-space(.),1,1))
+    ]">
+    <xsl:call-template name="string-ltrim">
+      <xsl:with-param name="string" select="." />
+    </xsl:call-template>
+  </xsl:template>
+
+  <!--contains(',;:)]', substring(normalize-space(.),1,1)-->
+
+  <!--  trim the whitespace from the text element directly following
+        a subscript and directly preceding another subscript if it starts
+        with a ', | ; | : | ) | ]' so that the subscript and the text
+        are positioned correctly -->
+  <xsl:template match="text()[
+      following-sibling::*[1][self::E][@T=51 or @T=52 or @T=53 or @T=54]
+      and
+      preceding-sibling::*[1][self::E][@T=51 or @T=52 or @T=53 or @T=54]
+      and
+      contains(',;:)]', substring(normalize-space(.),1,1))
+    ]">
+    <xsl:call-template name="string-trim">
+      <xsl:with-param name="string" select="." />
+    </xsl:call-template>
+  </xsl:template>
+
+
+  <xsl:template match="E[@T=51]">
+    <sup>
+      <xsl:apply-templates />
+    </sup>
+  </xsl:template>
+
+  <xsl:template match="E[@T=52]">
+    <sub>
+      <xsl:apply-templates />
+    </sub>
+  </xsl:template>
+
+  <xsl:template match="E[@T=53]">
+    <sup>
+      <em>
+        <xsl:apply-templates />
+      </em>
+    </sup>
+  </xsl:template>
+
+  <xsl:template match="E[@T=54]">
+    <sub>
+      <em>
+        <xsl:apply-templates />
+      </em>
+    </sub>
+  </xsl:template>
+
   <!--
     text italicised in header
     See page 9 here: http://www.gpo.gov/fdsys/pkg/FR-2015-01-22/pdf/2015-00344.pdf
   -->
   <xsl:template match="E[@T=7462]">
-    <xsl:call-template name="optional_preceding_whitespace" />
+    <!--<xsl:call-template name="optional_preceding_whitespace" />-->
     <em>
       <xsl:apply-templates />
     </em>
-    <xsl:call-template name="optional_following_whitespace" />
+    <!--<xsl:call-template name="optional_following_whitespace" />-->
   </xsl:template>
 
 </xsl:stylesheet>
--- spec/xslt/full_text/typeface_spec.rb
+++ spec/xslt/full_text/typeface_spec.rb
@@ -75,5 +75,142 @@ describe "XSLT::FullText::Typeface" do
         </p>
       HTML
     end
+
+    context "properly removing whitespace for subscripts and superscript" do
+      context "T=51 tags" do
+        it "covert to superscripts" do
+          process <<-XML
+            <P>
+              food and drinking water. Using DEEM-FCID
+              <E T="51">TM</E>
+              , acute dietary exposure
+            </P>
+          XML
+
+          expect_equivalent <<-HTML
+            <p id="p-1" data-page="1000">
+              food and drinking water. Using DEEM-FCID<sup>TM</sup>,
+              acute dietary exposure
+            </p>
+          HTML
+        end
+      end
+
+      context "T=52" do
+        it "converts to subscripts when they occur in a comma seperated row" do
+          process <<-XML
+            <P>
+              (thereby emitting additional PM, CO
+              <E T="52">2</E>
+              , SO
+              <E T="52">2</E>
+              , NO
+              <E T="52">X</E>
+              , and Hg). There are two
+            </P>
+          XML
+
+          expect_equivalent <<-HTML
+            <p id="p-1" data-page="1000">
+              (thereby emitting additional PM, CO<sub>2</sub>, SO<sub>2</sub>,
+              NO<sub>X</sub>, and Hg). There are two
+            </p>
+          HTML
+        end
+
+        it "coverts to subscripts when the last item does not have puntuation after it (comma, period, etc)" do
+          process <<-XML
+            <P>
+              particulate matter (PM
+              <E T="52">2.5</E>
+              ) on July 18, 1997 and on October 17, 2006. The CAA requires that each state, after a new or revised NAAQS is promulgated, review their SIPs to ensure that they meet infrastructure requirements. The State of North Dakota submitted certifications of their infrastructure SIP on August 12, 2010 and May 22, 2012 for the 2006 PM
+              <E T="52">2.5</E>
+              NAAQS. In addition, the State of North Dakota submitted a certification of their infrastructure SIP on May 25, 2012 for the 1997 PM
+              <E T="52">2.5</E>
+              NAAQS. EPA is also
+            </P>
+          XML
+
+          expect_equivalent <<-HTML
+            <p id="p-1" data-page="1000">
+              particulate matter (PM<sub>2.5</sub>) on July 18, 1997 and on
+              October 17, 2006. The CAA requires that each state, after a new
+              or revised NAAQS is promulgated, review their SIPs to ensure that
+              they meet infrastructure requirements. The State of North Dakota
+              submitted certifications of their infrastructure SIP on August
+              12, 2010 and May 22, 2012 for the 2006 PM<sub>2.5</sub> NAAQS.
+              In addition, the State of North Dakota submitted a certification
+              of their infrastructure SIP on May 25, 2012 for the 1997
+              PM<sub>2.5</sub> NAAQS. EPA is also
+            </p>
+          HTML
+        end
+      end
+
+      context "T=53" do
+        it "convert to italic superscript tags" do
+          process <<-XML
+            <P>
+              is the number of units produced for sale in the United States of each
+              <E T="03">i</E>
+              <E T="53">th</E>
+              unique footprint within each model
+            </P>
+          XML
+
+          expect_equivalent <<-HTML
+            <p id="p-1" data-page="1000">
+              is the number of units produced for sale in the United States of
+              each <em>i</em><sup><em>th</em></sup> unique footprint within each model
+            </p>
+          HTML
+        end
+      end
+
+      context "both T=52 and T=53" do
+        it "convert properly to both subscript and superscripts when used together" do
+          process <<-XML
+            <P>
+              <E T="03">PRODUCTION</E>
+              <E T="52">i</E>
+              is the number of units produced for sale in the United States of each
+              <E T="03">i</E>
+              <E T="53">th</E>
+              unique footprint within each model type produced for sale in the United States, and
+            </P>
+          XML
+
+          expect_equivalent <<-HTML
+            <p id="p-1" data-page="1000">
+              <em>PRODUCTION</em><sub>i</sub> is the number of units produced
+              for sale in the United States of each <em>i</em><sup><em>th</em></sup>
+              unique footprint within each model type produced for sale in the
+              United States, and
+            </p>
+          HTML
+        end
+      end
+
+      context "T=54" do
+        it "converts to an italic subscript tag" do
+          process <<-XML
+            <P>
+              produced for sale in the United States, and
+              <E T="03">TARGET</E>
+              <E T="54">i</E>
+              is the corresponding fuel economy target
+            </P>
+          XML
+
+          expect_equivalent <<-HTML
+            <p id="p-1" data-page="1000">
+              produced for sale in the United States, and
+              <em>TARGET</em><sub><em>i</em></sub> is the corresponding
+              fuel economy target
+            </p>
+          HTML
+        end
+      end
+    end
   end
 end
