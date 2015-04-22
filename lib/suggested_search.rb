a0568b8674ea4340cec2a06c894af1916b1fde51
Brandon Carag <brandon.carag@gmail.com>
2015-04-22 12:05:03 -0700

    Alter page to account for new suggested search api results.

--- lib/suggested_search.rb
+++ lib/suggested_search.rb
@@ -4,19 +4,4 @@ class SuggestedSearch < FederalRegister::SuggestedSearch
     FederalRegister::Document.search(:conditions => search_conditions).results
   end
 
-  def count_comments_open_for_comment
-    count = 0
-    facet_searches = FederalRegister::Facet::Document::Yearly.search(
-      conditions: {
-        publication_date: {
-          gte: Date.current - 2.years
-        },
-        comment_date: {
-          gte: Date.current
-        }
-      }.merge(search_conditions)
-    ).each {|year_result|count += year_result.count}
-    count
-  end
-
 end
