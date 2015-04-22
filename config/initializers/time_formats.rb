30ca48d51b0f576cf8ab85a3837b14447ae7a4b2
Peregrinator <bob.burbach@gmail.com>
2015-04-22 11:57:52 -0700

    Add some missing time formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -2,7 +2,8 @@ Date::DATE_FORMATS[:default] = "%m/%d/%Y"
 Date::DATE_FORMATS[:date] = "%m/%d/%Y"
 Date::DATE_FORMATS[:day_date] = "%A %d"
 Date::DATE_FORMATS[:month_year] = "%B %Y"
-Date::DATE_FORMATS[:year_month_day] = "%Y-%m-%d"
+Date::DATE_FORMATS[:ymd] = "%Y/%m/%d"
+Date::DATE_FORMATS[:year_month] = "%Y/%m"
 
 # Apr. 22nd, 2011
 Date::DATE_FORMATS[:shorter_ordinal] = lambda { |time| time.strftime("%b #{time.day.ordinalize}, %Y") }
@@ -20,3 +21,4 @@ Time::DATE_FORMATS[:datetime_with_zone] = "%m/%d/%Y at %I:%M %p %Z"
 # "Friday, April 22nd, 2011 at 1:30 PM"
 Time::DATE_FORMATS[:formal_with_time] = lambda { |time| time.strftime("%A, %B #{time.day.ordinalize}, %Y at %I:%M %p") }
 Time::DATE_FORMATS[:time_then_date] = lambda { |time| time.strftime("%I:%M %p, on %A, %B #{time.day.ordinalize}, %Y") }
+
