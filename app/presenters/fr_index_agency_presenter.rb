b8891f3f8d74d41db1b3afaccab0e29368145063
Brandon Carag <brandon.carag@gmail.com>
2015-04-30 21:22:49 -0700

    Create DocumentType subclass for readability and implement subject counting totals.

--- app/presenters/fr_index_agency_presenter.rb
+++ app/presenters/fr_index_agency_presenter.rb
@@ -1,6 +1,3 @@
-# reload!; @presenter = FrIndexAgencyPresenter.new(2014,'treasury-department')
-# slug, agency = @presenter.agency.first
-
 class FrIndexAgencyPresenter
   attr_reader :year, :agency_slug, :document_index
 
@@ -10,15 +7,46 @@ class FrIndexAgencyPresenter
     @document_index = HTTParty.get(url)
   end
 
+  class DocumentType
+    vattr_initialize [
+      :name,
+      :type,
+      :document_count,
+      :subject_count
+    ]
+  end
+
+  def document_type_names #TODO: Locate definitive place for this info.
+    {
+      "NOTICE" => "Notice",
+      "PRESDOCU" => "Presidential Document",
+      "PRORULE" => "Proposed Rule",
+      "RULE" => "Rule",
+      "UNKNOWN" => "Unknown",
+    }
+  end
+
+  def document_types
+    document_index["document_categories"].map do |category|
+      DocumentType.new(
+        name: document_type_names[category["name"]], #TODO: Change source data to definitive location
+        type: category["name"],
+        document_count: document_type_counts[category["name"]],
+        subject_count: category["documents"].group_by{|d|d["subject_1"]}.keys.size,
+      )
+    end
+  end
+
   def agency_name
     document_index["name"]
   end
 
   def total_document_count
-    @total_count ||= document_type_counts.inject(0) do |sum, doc_facet|
-      sum+=doc_facet.count
-      sum
+    total=0
+    @total_count ||= document_type_counts.each do |type, count|
+      total+=count
     end
+    total
   end
 
   def document_type_counts
@@ -31,7 +59,18 @@ class FrIndexAgencyPresenter
           },
           agencies: [agency_slug]
         }
-      )
+      ).inject({}) do |hsh, facet|
+          hsh[facet.slug]= facet.count
+          hsh
+        end
+  end
+
+  def doc_counts_by_category
+    hsh = {}
+    document_index["document_categories"].each do |category|
+      hsh[category["name"]] = category["documents"].group_by{|d|d["subject_1"]}.keys.size
+    end
+    hsh
   end
 
   def url
@@ -45,8 +84,6 @@ class FrIndexAgencyPresenter
   def agency
     @agency ||= {agency_slug => Agency.new(document_index, self) }
   end
-
-
 
   def documents
     @documents ||= Document.search(
@@ -72,6 +109,10 @@ class FrIndexAgencyPresenter
       }
     ).results.map{|d| DocumentDecorator.decorate(d)}
   end
+
+
+
+  # reload!;FrIndexAgencyPresenter.new(2014,"treasury-department").document_type_counts
 
   class Agency < TableOfContentsPresenter::Agency #TODO: Move TableOfContentsPresenter::Agency into
     #higher class to maximize re-use.
