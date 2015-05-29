a5c2103448ed51eb79f9b28360dcfaba500e25ac
Peregrinator <bob.burbach@gmail.com>
2015-05-29 11:09:41 -0700

    Add icon attribute to sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,11 +1,11 @@
 class Section < ActiveHash::Base
    self.data = [
-    {id: 1, title: 'Money', slug: 'money'},
-    {id: 2, title: 'Environment', slug: 'environment'},
-    {id: 3, title: 'World', slug: 'world'},
-    {id: 4, title: 'Science and Technology', slug: 'science-and-technology'},
-    {id: 5, title: 'Business and Industry', slug: 'business-and-industry'},
-    {id: 6, title: 'Health and Public Welfare', slug: 'health-and-public-welfare'},
+    {id: 1, title: 'Money', slug: 'money', icon: 'Coins-dollaralt'},
+    {id: 2, title: 'Environment', slug: 'environment', icon: 'Eco'},
+    {id: 3, title: 'World', slug: 'world', icon: 'globe'},
+    {id: 4, title: 'Science and Technology', slug: 'science-and-technology', icon: 'Lab'},
+    {id: 5, title: 'Business and Industry', slug: 'business-and-industry', icon: 'Factory'},
+    {id: 6, title: 'Health and Public Welfare', slug: 'health-and-public-welfare', icon: 'Medicine'},
    ]
 
   def self.slugs
