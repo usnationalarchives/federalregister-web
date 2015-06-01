b959bcb54026a6f624d56e7f07450d880069168f
Peregrinator <bob.burbach@gmail.com>
2015-06-01 13:20:36 -0700

    Disable click on the utility bar such that we’re not changing styles on click events (they were bubbling up to the hover event).

--- app/assets/javascripts/utility_bar.js
+++ app/assets/javascripts/utility_bar.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-  $('ul.doc-nav li').hover(function() {
+  $('ul.doc-nav > li').hover(function() {
     $(this).find('.dropdown-menu').stop(true, true).show();
     $(this).addClass('open');
   }, function() {
     $(this).find('.dropdown-menu').stop(true, true).hide();
     $(this).removeClass('open');
+  });
+
+  $('ul.doc-nav > li').click(function(e) {
+    // this shouldn't be neccessary but is for some reason...
+    if( e.toElement.tagName !== 'A' ) {
+      e.preventDefault();
+    }
+    e.stopPropagation();
   });
 
   if( $('.doc-nav-wrapper').length > 0 ) {
