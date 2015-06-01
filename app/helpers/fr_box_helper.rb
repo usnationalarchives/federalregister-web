82083c410d99e21d1fed1dc5145121e4cd78ac90
Peregrinator <bob.burbach@gmail.com>
2015-06-01 12:43:44 -0700

    Pass the box type along so that we can properly set the column size on smaller fr boxes.

--- app/helpers/fr_box_helper.rb
+++ app/helpers/fr_box_helper.rb
@@ -1,23 +1,27 @@
 module FrBoxHelper
 
   def fr_box(title, type, options={}, &block)
+    box_type = options.fetch(:box_type) { :large }
     box_options = options.fetch(:box_html) { Hash.new }
     header_options = options.fetch(:header) { Hash.new }
     content_block_options = options.fetch(:content_block_html) { Hash.new }
 
     content_tag(:div, class: "fr-box #{css_class_by_type(type)} #{box_options[:class]}", role: box_options[:role]) do
-      fr_box_header(title, type, default_header_options.merge!(header_options)) +
+      fr_box_header(title, type, box_type, default_header_options.merge!(header_options)) +
         content_tag(:div, capture(&block), class: "content-block #{content_block_options[:class]}") +
         fr_box_footer(title, type)
     end
   end
 
   def fr_box_small(title, type, options={}, &block)
-    #inject the small boc class
+    #inject the small box class
     if options[:box_html] && options[:box_html][:class]
       options[:box_html][:class] += "fr-box-small"
     else
-      options.deep_merge!(box_html: {class: "fr-box-small"})
+      options.deep_merge!(
+        box_html: {class: "fr-box-small"},
+        box_type: :small
+      )
     end
 
     fr_box(title, type, options, &block)
@@ -30,10 +34,10 @@ module FrBoxHelper
     }
   end
 
-  def fr_box_header(title, type, options)
+  def fr_box_header(title, type, box_type, options)
     content_tag(:div, class: "fr-seal-block fr-seal-block-header") do
       content_tag(:div, class: "fr-seal-content") do
-        description = options[:hover] ? fr_box_header_description(type, options) : "".html_safe
+        description = options[:hover] ? fr_box_header_description(type, box_type, options) : "".html_safe
 
         content_tag(:h6, title) +
           description
@@ -41,12 +45,14 @@ module FrBoxHelper
     end
   end
 
-  def fr_box_header_description(type, options={})
+  def fr_box_header_description(type, box_type, options={})
+    col_span = box_type == :small ? 12 : 9
+
     content_tag(:div, class: "row fr-seal-meta") do
       html = options[:seal] ? fr_box_seal(type) : "".html_safe
 
       html +
-      content_tag(:div, class: "fr-seal-desc col-md-9 col-xs-9") do
+      content_tag(:div, class: "fr-seal-desc col-md-#{col_span} col-xs-#{col_span}") do
         if options[:description]
           options[:description].html_safe
         else
