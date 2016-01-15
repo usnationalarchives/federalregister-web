126ede6dadc25666d2015ae792b6e48794a31476
Peregrinator <bob.burbach@gmail.com>
2016-01-15 11:02:46 -0800

    Handle api failure more gracefully so that the app would start in an api outage.

--- lib/suggested_search_contraint.rb
+++ lib/suggested_search_contraint.rb
@@ -1,9 +1,19 @@
 class SuggestedSearchContraint
-  def initialize
-    @suggested_search_slugs = Rails.env.test? ? [] : SuggestedSearch.slugs
+  def matches?(request)
+    suggested_search_slugs.include?(request.path_parameters[:slug])
   end
 
-  def matches?(request)
-    @suggested_search_slugs.include?(request.path_parameters[:slug])
+  def suggested_search_slugs
+    return @suggested_search_slugs if @suggested_search_slugs
+
+    @suggested_search_slugs = []
+
+    unless Rails.env.test?
+      begin
+        @suggested_search_slugs = SuggestedSearch.slugs
+      rescue Exception => e
+        Honeybadger.notify(e)
+      end
+    end
   end
 end
