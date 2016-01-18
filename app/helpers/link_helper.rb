714e180a985d9ec966d671eaf7c8bf11ea6af5a9
Peregrinator <bob.burbach@gmail.com>
2016-01-18 15:07:20 -0800

    Link up facebook and twitter share links

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,13 +1,17 @@
 module LinkHelper
-  def link_to_twitter(status, link_text = "Twitter", icons=true)
+  def link_to_twitter(link_text, status)
     href = "http://twitter.com/home?status=#{CGI.escape status}"
-    link_class = icons ? 'button list social twitter tip_over' : 'twitter'
-    link_to(link_text, href, :target => :blank, :title => 'Share on Twitter', :class => link_class)
+    link_to link_text, href,
+      class: 'social twitter',
+      target: :blank,
+      title: 'Share on Twitter'
   end
 
-  def link_to_facebook(url, title, link_text="Facebook", icons=true)
+  def link_to_facebook(link_text, url, title)
     href = "http://www.facebook.com/sharer.php?u=#{CGI.escape url}&t=#{CGI.escape title}"
-    link_class = icons ? 'button list fb_link social facebook tip_over' : 'facebook'
-    link_to(link_text, href, :target => :blank, :class => link_class, :title => "Share on Facebook")
+    link_to link_text, href,
+      class: 'social facebook',
+      target: :blank,
+      title: "Share on Facebook"
   end
 end
