6847187eb5e767729cbf7348234d9384596e6e7c
Peregrinator <bob.burbach@gmail.com>
2016-01-20 10:14:13 -0800

    Update link to US Code for PI documents. Share the PI legal notice warning between both utility bars for future consistency.

---
+++ app/helpers/public_inspection_helper.rb
@@ -0,0 +1,15 @@
+module PublicInspectionHelper
+  def public_inspection_uscode_link
+    link_to '44 U.S.C. 1503 & 1507',
+      "http://www.gpo.gov/fdsys/pkg/USCODE-2009-title44/html/USCODE-2009-title44-chap15.htm"
+  end
+
+  def utility_bar_warning_text
+    "If you are using public inspection listings for legal research, you
+    should verify the contents of the documents against a final, official
+    edition of the Federal Register. <strong>Only official editions of the
+    Federal Register provide legal notice to the public and judicial notice
+    to the courts under #{public_inspection_uscode_link}.</strong>
+    Learn more #{link_to 'here', public_inspection_learn_path}.".html_safe
+  end
+end
