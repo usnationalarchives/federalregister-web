ca70762a889673381f6835fea1fb338677d75c72
Peregrinator <bob.burbach@gmail.com>
2016-02-03 17:16:07 -0800

    Rails caches this on startup so no need for memoization. Raise the error in dev otherwise notify honeybadger.

--- lib/suggested_search_contraint.rb
+++ lib/suggested_search_contraint.rb
@@ -4,16 +4,20 @@ class SuggestedSearchContraint
   end
 
   def suggested_search_slugs
-    return @suggested_search_slugs if @suggested_search_slugs
-
-    @suggested_search_slugs = []
+    suggested_search_slugs = []
 
     unless Rails.env.test?
       begin
-        @suggested_search_slugs = SuggestedSearch.slugs
+        suggested_search_slugs = SuggestedSearch.slugs
       rescue StandardError => e
-        Honeybadger.notify(e)
+        if Rails.env.development?
+          raise e
+        else
+          Honeybadger.notify(e)
+        end
       end
     end
+
+    suggested_search_slugs
   end
 end
