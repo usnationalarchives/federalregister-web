8f039f1aae24ee5cc1388877e3b86015c9c9b502
Peregrinator <bob.burbach@gmail.com>
2016-02-19 11:40:19 -0800

    Fix query condition where the merge was missing the conditions hash resulting in a search param that was in the wrong place and thus ignored.

--- app/searches/query_conditions/public_inspection_document_conditions.rb
+++ app/searches/query_conditions/public_inspection_document_conditions.rb
@@ -17,13 +17,13 @@ class QueryConditions::PublicInspectionDocumentConditions < QueryConditions
 
   def self.special_filings_available_on(date)
     special_filing.deep_merge({
-      available_on: iso(date)
+      conditions: {available_on: iso(date)}
     })
   end
 
   def self.regular_filings_available_on(date)
     regular_filing.deep_merge({
-      available_on: iso(date)
+      conditions: {available_on: iso(date)}
     })
   end
 end
