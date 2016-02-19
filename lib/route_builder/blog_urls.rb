b9b90c8804b99e591a8f43e1337fabf6f430b31a
Peregrinator <bob.burbach@gmail.com>
2016-02-19 13:58:17 -0800

    Fix PI help urls. Fix links to policy and pi in mailers. Update links to developer tools pages. Slight update to utility bar text.

--- lib/route_builder/blog_urls.rb
+++ lib/route_builder/blog_urls.rb
@@ -2,10 +2,10 @@ module RouteBuilder::BlogUrls
   extend RouteBuilder::Utils
 
   def public_inspection_learn_path
-    "/reader-aids/using-federalregister-gov/understanding-public-inspection-documents"
+    "/reader-aids/using-federalregister-gov/understanding-public-inspection"
   end
 
   def public_inspection_table_of_effective_dates_path
-    "/reader-aids/using-federalregister-gov/understanding-public-inspection-documents/table-of-effective-dates-time-periods"
+    "/reader-aids/using-federalregister-gov/understanding-public-inspection/table-of-effective-dates-time-periods"
   end
 end
