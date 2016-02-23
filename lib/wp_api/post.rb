53758bb7f729267d7b0b2d31abc970965aeeaf98
Peregrinator <bob.burbach@gmail.com>
2016-02-23 14:56:38 -0800

    Include the forward slashes when substituting so that we don’t over match the word blog elsewhere in the title.

--- lib/wp_api/post.rb
+++ lib/wp_api/post.rb
@@ -1,5 +1,5 @@
 class WpApi::Post < WpApi::Content
   def path(section_identifier='office-of-the-federal-register-blog')
-    link.gsub('blog', "reader-aids/#{section_identifier}")
+    link.gsub('/blog/', "/reader-aids/#{section_identifier}/")
   end
 end
