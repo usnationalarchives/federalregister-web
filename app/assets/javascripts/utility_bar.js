bd1609075552d62e0628af6ce1cb60b004e6f31f
Peregrinator <bob.burbach@gmail.com>
2016-04-07 15:20:31 -0700

    Add missing semicolon

--- app/assets/javascripts/utility_bar.js
+++ app/assets/javascripts/utility_bar.js
@@ -19,12 +19,12 @@ $(document).ready(function() {
     // set the height of the nav wrapper after we've loaded web fonts
     // otherwise the height won't be correct
     $('body').on('typekit-active', function() {
-      var frBox = $('.doc-nav-wrapper').siblings('.fr-box').first()
+      var frBox = $('.doc-nav-wrapper').siblings('.fr-box').first();
 
       $('.doc-document ul.doc-nav').sticky({
         context: '.doc-content.with-utility-bar'
       });
-      
+
       // ideally we don't want a timeout here but semantic-ui sticky
       // makes the wrapper bigger than it should be - so we wait for it
       // to finish before fixing.
