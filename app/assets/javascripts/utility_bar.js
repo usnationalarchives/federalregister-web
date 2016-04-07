745ae91cc262d7e7553cd5b0e3916eca95b87074
Peregrinator <bob.burbach@gmail.com>
2016-04-07 16:37:31 -0700

    Add the sticky before the typescript fonts are loaded (fixes cases when active isn’t firing) and refresh it when they do. Add a background to the utility bar so that side scrolling when the utility bar is ‘fixed’ isn’t completely broken.

--- app/assets/javascripts/utility_bar.js
+++ app/assets/javascripts/utility_bar.js
@@ -16,19 +16,20 @@ $(document).ready(function() {
   });
 
   if( $('.doc-nav-wrapper').length > 0 ) {
+    $('.doc-document ul.doc-nav').sticky({
+      context: '.doc-content.with-utility-bar'
+    });
+
     // set the height of the nav wrapper after we've loaded web fonts
     // otherwise the height won't be correct
     $('body').on('typekit-active', function() {
-      var frBox = $('.doc-nav-wrapper').siblings('.fr-box').first();
-
-      $('.doc-document ul.doc-nav').sticky({
-        context: '.doc-content.with-utility-bar'
-      });
-
+      $('.doc-document ul.doc-nav').sticky('refresh');
+      
       // ideally we don't want a timeout here but semantic-ui sticky
       // makes the wrapper bigger than it should be - so we wait for it
       // to finish before fixing.
       setTimeout(function(){
+        var frBox = $('.doc-nav-wrapper').siblings('.fr-box').first();
         $('.doc-nav-wrapper').outerHeight(frBox.height() + 'px');
       }, 1000);
     });
