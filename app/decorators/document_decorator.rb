b19c7fae44977cc5b57cecef4c53486f7f3b97b0
Brandon Carag <brandon.carag@gmail.com>
2016-04-22 09:15:33 -0700

    Replace deprecated #agency_names method call with #agency_name_sentence (which uses agencies without parents)

--- app/decorators/document_decorator.rb
+++ app/decorators/document_decorator.rb
@@ -81,7 +81,7 @@ class DocumentDecorator < ApplicationDecorator
     return @document_metadata_description if @document_metadata_description
 
     description = "#{type.with_indefinite_article(true)} by " +
-    content_tag(:span, agency_names, class: "agencies") +
+    content_tag(:span, agency_name_sentence, class: "agencies") +
     " on " +
     h.link_to(
       h.date_tag(publication_date, datetime: publication_date),
