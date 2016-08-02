b992d64351c4df861afd1327faac05accc64a6c2
Peregrinator <bob.burbach@gmail.com>
2016-08-02 13:32:57 -0700

    Add cache header to suggested searches

--- app/controllers/suggested_searches_controller.rb
+++ app/controllers/suggested_searches_controller.rb
@@ -2,6 +2,7 @@ class SuggestedSearchesController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def show
+    cache_for 1.day
     @presenter = SuggestedSearchPresenter.new(params[:slug])
 
   rescue SuggestedSearchPresenter::InvalidSuggestedSearch
