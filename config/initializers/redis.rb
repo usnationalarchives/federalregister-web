9c6fce5226004e63862897e533ec5a37291b1a11
Peregrinator <bob.burbach@gmail.com>
2016-08-09 10:56:31 -0700

    Update our configuration of redis to reconnect when forked by passenger. Ensure we explicitly tell Resque what redis to use.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,30 @@
-REDIS_CONFIG = YAML.load_file(Rails.root+'config/redis.yml')[Rails.env].freeze
+REDIS_CONFIG = YAML.load_file(Rails.root + 'config/redis.yml')[Rails.env].freeze
 
-redis = Redis.new(
+REDIS_CONNECTION_SETTINGS = {
+  :db   => REDIS_CONFIG['db'],
   :host => REDIS_CONFIG['host'],
   :port => REDIS_CONFIG['port']
-)
+}
+
+redis = Redis.new(REDIS_CONNECTION_SETTINGS)
 
 $redis = Redis::Namespace.new(
   REDIS_CONFIG['namespace'],
   :redis => redis
 )
 
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # We're in smart spawning mode.
+    if forked
+      $redis.client.disconnect
+      $redis = Redis::Namespace.new(
+        REDIS_CONFIG['namespace'],
+        :redis => Redis.new(REDIS_CONNECTION_SETTINGS)
+      )
+    end
+  end
+end
+
 HTTParty::HTTPCache.redis = $redis
+Resque.redis = $redis
