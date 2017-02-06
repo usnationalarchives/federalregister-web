777847dbec3cc40ee83cf518e5ab3082d480ec09
Peregrinator <bob.burbach@gmail.com>
2017-02-06 11:50:40 -0800

    Don’t require screenshot for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
 
-require 'capybara-screenshot/rspec'
+#require 'capybara-screenshot/rspec'
 
 require "email_spec"
 
