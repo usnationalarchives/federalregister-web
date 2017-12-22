bfadd82a07c9b86c6a6a23ee6b36cef8c0e150fd
Brandon Carag <brandon@criticaljuncture.org>
2017-12-22 10:24:38 -0800

    Add specs for Citations#fr page and reorganize fr integration spec.  Update related VCR recordings

--- spec/features/fr_archives_integration_spec.rb
+++ spec/features/fr_archives_integration_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 feature "FR Archives Integration", :no_ci do
-  #NOTE: Tests /documents/search/suggestions ESI which powers the top informational section of the document search page at /documents/search?conditions%5Bterm%5D=20+FR+60602
+
+  context "Documents search page" do
+    #NOTE: Tests relate to the /documents/search/suggestions ESI, which powers the informational section at the top of the document search page
 
     scenario "The correct citation is returned", :vcr do
       visit '/documents/search/suggestions?conditions%5Bterm%5D=35+FR+118'
@@ -17,5 +19,26 @@ feature "FR Archives Integration", :no_ci do
       visit '/documents/search/suggestions?conditions%5Bterm%5D=82+FR+60602'
       expect(page).to have_content("We've found the following documents")
     end
+  end
+
+  context "Citation#fr page" do
+
+    scenario "Citation that references an issue before the archives", :vcr do
+      visit '/citation/1-FR-1'
+      expect(page).to have_content("No documents found")
+      expect(page).to have_content("Older volumes may be available through a Federal Depository Library")
+    end
+
+    scenario "Citation that references an issue that falls within the archives", :vcr do
+      visit '/citation/35-FR-117'
+      expect(page).to have_text("An issue was located for citation")
+      expect(page).to have_content("35 FR 117 Size-optimized Pages")
+    end
+
+    scenario "Citation that falls after the archives", :vcr do
+      visit '/citation/82-FR-60602'
+      expect(page).to have_content("Multiple documents found")
+    end
+  end
 
 end
