e8b8c740e0177cd14abdc46377ef3c2ddcd83a80
Brandon Carag <brandon@criticaljuncture.org>
2018-02-20 13:19:07 -0800

    Ensure API call to FR profile for emails is using the correct environment variable for password

--- lib/ecfr/user_email_result_set.rb
+++ lib/ecfr/user_email_result_set.rb
@@ -4,7 +4,7 @@ class Ecfr::UserEmailResultSet
     conn = Faraday.new(:url => Settings.services.fr_profile_url)
     conn.basic_auth(
       SECRETS["fr_profile"]["basic_username"],
-      SECRETS["fr_profile"]["basic_username"],
+      SECRETS["fr_profile"]["basic_password"],
     )
     response = conn.post '/api/profile/v1/emails', {ids: Array.wrap(user_ids)}
     JSON.parse(response.body)
