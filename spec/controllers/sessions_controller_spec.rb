e4890aade53dc6e8df873587c91d79cc34c00b13
Brandon Carag <brandon@criticaljuncture.org>
2018-02-21 16:08:27 -0800

    Add session creation and destruction specs

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,52 @@
+describe SessionsController do
+  let(:session_user_details) {
+    OmniAuth::AuthHash.new(
+      email:           stubbed_auth_hash["extra"]["raw_info"]["email"],
+      email_confirmed: stubbed_auth_hash["extra"]["raw_info"]["email_confirmed"],
+      token:           stubbed_auth_hash["credentials"]["token"],
+      id:              stubbed_auth_hash["uid"]
+    )
+  }
+
+  context "#create" do
+    it "Creates the user session properly" do
+      request.env["omniauth.auth"] = stubbed_auth_hash
+
+      # Removes user email from the Sendgrid bounce list
+      expect_any_instance_of(SendgridClient).to receive(:remove_from_bounce_list).
+        once.
+        with(stubbed_auth_hash[:extra][:raw_info][:email])
+
+      get :create
+
+      # Sets cookie JS and varnish relies upon
+      expect(cookies["expect_signed_in"]).to eq "1"
+
+      # Sets the correct values in the user session
+      expect(session[:user_details]).to eq(session_user_details)
+    end
+  end
+
+  context "#destroy" do
+    before(:each) do
+      session[:user_details]      = session_user_details
+      cookies["expect_signed_in"] = "1"
+    end
+
+    it "AJAX requests delete the session and remove the signed in cookie" do
+      xhr :get, :destroy
+
+      expect(session).to eq({})
+      expect(cookies["expect_signed_in"]).to eq "0"
+    end
+
+    it "Non-AJAX requests do not delete the session or remove the cookie, but only redirect to FR Profile" do
+      get :destroy
+
+      expect(session[:user_details]).to eq(session_user_details)
+      expect(cookies["expect_signed_in"]).to eq "1"
+    end
+
+  end
+
+end
