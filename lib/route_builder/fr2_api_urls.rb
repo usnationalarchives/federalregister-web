bb0372fcb3725344f278535167234c17f3772887
Peregrinator <bob.burbach@gmail.com>
2018-03-03 13:52:28 -0800

    Add _path options in addition to _url

--- lib/route_builder/fr2_api_urls.rb
+++ lib/route_builder/fr2_api_urls.rb
@@ -2,7 +2,7 @@ module RouteBuilder::Fr2ApiUrls
   extend RouteBuilder::Utils
 
   ### DOCUMENTS
-  def document_api_url(document, options, params={})
+  def document_api_path(document, options, params={})
     path = "documents/#{document.document_number}"
 
     # document api path doesn't support format currently
@@ -10,10 +10,14 @@ module RouteBuilder::Fr2ApiUrls
       path += ".#{options[:format]}"
     end
 
-    fr2_api_url_for(path, params)
+    fr2_api_path_for(path, params)
   end
 
-  def documents_api_url(documents, options, params={})
+  def document_api_url(document, options, params={})
+    fr2_api_urlify(documens_api_path(documents, options, params))
+  end
+
+  def documents_api_path(documents, options, params={})
     document_numbers = documents.map{|d| d.document_numbers}.join(',')
     path = "documents/#{document_numbers}"
 
@@ -22,36 +26,52 @@ module RouteBuilder::Fr2ApiUrls
       path += ".#{options[:format]}"
     end
 
-    fr2_api_url_for(path, params)
+    fr2_api_path_for(path, params)
   end
 
-  def documents_search_api_url(params, options)
+  def documents_api_url(documents, options, params={})
+    fr2_api_urlify(documents_api_path(documents, options, params))
+  end
+
+  def documents_search_api_path(params, options)
     path = "documents"
 
     if options && options[:format]
       path += ".#{options[:format]}"
     end
 
-    fr2_api_url_for(path, params)
+    fr2_api_path_for(path, params)
+  end
+
+  def documents_search_api_url(params, options)
+    fr2_api_urlify(documents_search_api_path(params, options))
   end
 
   ### PUBLIC INSPECTION DOCUMENTS
-  def public_inspection_search_api_url(params, options)
+  def public_inspection_search_api_path(params, options)
     path = "public-inspection-documents"
 
     if options && options[:format]
       path += ".#{options[:format]}"
     end
 
-    fr2_api_url_for(path, params)
+    fr2_api_path_for(path, params)
+  end
+
+  def public_inspection_search_api_url(params, options)
+    fr2_api_urlify(public_inspection_search_api_path(params, options))
   end
 
   private
 
-  def fr2_api_url_for(end_point, params)
-    arr = ["#{Settings.federal_register.api_url}/#{end_point}"]
+  def fr2_api_path_for(end_point, params)
+    arr = ["/#{end_point}"]
     arr << params.to_param unless params.blank?
 
     arr.join('?')
+  end
+
+  def fr2_api_urlify(path)
+    "#{Settings.federal_register.api_url}/#{path}"
   end
 end
