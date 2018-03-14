db699c095d6829fef4b30cbd27369bb9770a5bb0
Peregrinator <bob.burbach@gmail.com>
2018-03-14 15:24:22 -0700

    The Faraday url key only uses the host not the full path. Our setting contains the full path so we want to use that here.

--- lib/ecfr/user_email_result_set.rb
+++ lib/ecfr/user_email_result_set.rb
@@ -1,12 +1,12 @@
 class Ecfr::UserEmailResultSet
 
   def self.get_user_emails(user_ids)
-    conn = Faraday.new(:url => Settings.services.fr_profile_url)
+    conn = Faraday.new
     conn.basic_auth(
       SECRETS["fr_profile"]["basic_username"],
       SECRETS["fr_profile"]["basic_password"],
     )
-    response = conn.post '/api/profile/v1/emails', {ids: Array.wrap(user_ids)}
+    response = conn.post "#{Settings.services.fr_profile_url}/api/profile/v1/emails", {ids: Array.wrap(user_ids)}
     JSON.parse(response.body)
   end
 
