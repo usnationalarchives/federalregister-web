bfc1342020c1fd1c4575e38111d2c15ce8f27e93
Peregrinator <bob.burbach@gmail.com>
2018-07-12 12:58:55 -0700

    Change error message when encountering Regulations.gov rate limits for a more clear user experience.

--- lib/regulations_dot_gov/client.rb
+++ lib/regulations_dot_gov/client.rb
@@ -226,6 +226,7 @@ class RegulationsDotGov::Client
   def self.post(url, options)
     #query = options.fetch(:query){ Hash.new }
     #options[:query] = query.merge!(:api_key => api_key)
+    reg_gov_url = url
     url = url + "?api_key=#{api_key}"
 
     begin
@@ -237,7 +238,8 @@ class RegulationsDotGov::Client
       when 400, 406
         raise InvalidSubmission.new( stringify_response(response), response.code)
       when 429
-        raise OverRateLimit.new( stringify_response(response), response.code)
+        message = "We were unable to successfully submit your comment at this time because Regulations.gov has received too many comments in the last hour. Please try submitting your comment again later or attempt to comment directly via Regulations.gov: #{reg_gov_url}, or via any other method described in the document."
+        raise OverRateLimit.new(message, response.code)
       when 500, 502, 503
         raise ServerError.new( stringify_response(response), response.code)
       else
