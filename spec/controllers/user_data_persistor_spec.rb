0088abff8e06b372cfb1270695b95cf7edd4289a
Peregrinator <bob.burbach@gmail.com>
2018-12-19 13:18:53 -0800

    Refactor spec to use the rspec abstract controller setup. This avoids issues with needing to inject routes for our own test controller.

--- spec/controllers/user_data_persistor_spec.rb
+++ spec/controllers/user_data_persistor_spec.rb
@@ -1,25 +1,15 @@
 require 'spec_helper'
 
-# Create TestHarnessController to test UserDataPersistor in Isolation
-class TestHarnessController < ApplicationController
-  include UserDataPersistor
-
-  def test
-    persist_user_data
-  end
-end
-
-describe TestHarnessController do
-  before(:each) do
-    add_test_harness_route('/test', "test_harness#test")
-    User.current = nil
-  end
-
-  def add_test_harness_route(route, controller_action)
-    test_routes = Proc.new do
-      get route, to: controller_action
+describe SessionsController do
+  # create our own abtract controller with action for testing
+  controller do
+    def new
+      persist_user_data
     end
-    Rails.application.routes.eval_block(test_routes)
+  end
+
+  before(:each) do
+    User.current = nil
   end
 
   def authenticated_session
@@ -34,18 +24,19 @@ describe TestHarnessController do
 
   describe "Clippings" do
     before(:each) do
-      Clipping.any_instance.stub(:document).and_return(double)
+      allow_any_instance_of(Clipping).to receive(:document).and_return(double)
     end
 
     it "persists a document if a document number is saved in the cookie" do
       cookies[:document_numbers] = [{'2014-13781' => ['my-clippings']}].to_json
-      get :test, nil, authenticated_session
+      get :new, nil, authenticated_session
 
       clipping = Clipping.first
+
       {
         document_number: '2014-13781',
-        user_id:         authenticated_session[:user_details]["sub"],
-        folder_id:       nil
+        user_id: authenticated_session[:user_details]["sub"],
+        folder_id: nil
       }.each do |attribute, value|
         expect(clipping[attribute]).to eq(value)
       end
@@ -59,23 +50,22 @@ describe TestHarnessController do
         :persist_comment_data,
       ]
       validation_methods.each do |validation_method|
-        Comment.any_instance.stub(validation_method).and_return(true)
+        allow_any_instance_of(Clipping).to receive(validation_method).and_return(true)
       end
-      comment
     end
 
-    let(:comment) do
+    let!(:comment) do
       create(:comment_skipped_validations,
         comment_tracking_number: 123,
       )
     end
 
     it "if a comment tracking number and secret are stored in the session, and a matching comment exists in the database, the CommentMailer is called" do
-      Ecfr::UserEmailResultSet.stub(:get_user_emails).and_return(values: ['john_doe@example.com'])
+      allow(Ecfr::UserEmailResultSet).to receive(:get_user_emails).and_return(values: ['john_doe@example.com'])
       CommentMailer.stub_chain(:comment_copy, :deliver)
       expect(CommentMailer).to receive(:comment_copy)
 
-      get :test, nil, authenticated_session.merge(
+      get :new, nil, authenticated_session.merge(
         comment_tracking_number: 123,
         comment_secret: 'abcd1234',
       )
@@ -83,7 +73,7 @@ describe TestHarnessController do
 
     it "if a comment cannot be located the UserDataPersistor does not fail" do
       expect {
-        get :test, nil, authenticated_session.merge(
+        get :new, nil, authenticated_session.merge(
           comment_tracking_number:        77777,
           comment_secret:                 88888,
           followup_document_notification: "1"
@@ -99,7 +89,7 @@ describe TestHarnessController do
         token:        "abcd1234",
         user_id:      nil
       )
-      get :test, nil, authenticated_session.merge(
+      get :new, nil, authenticated_session.merge(
         subscription_token: subscription.token
       )
 
