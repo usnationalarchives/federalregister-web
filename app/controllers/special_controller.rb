e4acb5d00709aa3da71849eddb2e8ceb42a55408
Peregrinator <bob.burbach@gmail.com>
2019-01-04 11:38:31 -0800

    Use .where instead of deprecated .scoped

--- app/controllers/special_controller.rb
+++ app/controllers/special_controller.rb
@@ -4,8 +4,8 @@ class SpecialController < ApplicationController
 
   def user_utils
     if user_signed_in?
-      @clipboard_clippings = Clipping.scoped(:conditions => {:folder_id => nil, :user_id => current_user.id}).with_preloaded_articles || []
-      @folders = FolderDecorator.decorate( Folder.scoped(:conditions => {:creator_id => current_user.id}).all )
+      @clipboard_clippings = Clipping.where(folder_id: nil, user_id: current_user.id).with_preloaded_articles || []
+      @folders = FolderDecorator.decorate( Folder.where(creator_id: current_user.id).all )
     elsif cookies[:document_numbers].present?
       @clipboard_clippings = Clipping.all_preloaded_from_cookie( cookies[:document_numbers] ) || []
       @folders   = []
