df58eaba3a626afebe93fef0b76ced93e5f945bb
Peregrinator <bob.burbach@gmail.com>
2019-01-24 11:56:45 -0800

    Update to rails 5 style schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,11 +8,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20190307191045) do
+ActiveRecord::Schema.define(version: 20190307191045) do
 
-  create_table "clippings", :force => true do |t|
+  create_table "clippings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.string   "document_number"
     t.integer  "position"
@@ -22,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20190307191045) do
     t.integer  "folder_id"
   end
 
-  create_table "comment_attachments", :force => true do |t|
+  create_table "comment_attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "token"
     t.string   "attachment"
     t.string   "attachment_md5"
@@ -31,13 +30,12 @@ ActiveRecord::Schema.define(:version => 20190307191045) do
     t.integer  "file_size"
     t.string   "content_type"
     t.string   "original_file_name"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["created_at"], name: "index_comment_attachments_on_created_at", using: :btree
   end
 
-  add_index "comment_attachments", ["created_at"], :name => "index_comment_attachments_on_created_at"
-
-  create_table "comments", :force => true do |t|
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.string   "document_number"
     t.string   "comment_tracking_number"
@@ -46,31 +44,30 @@ ActiveRecord::Schema.define(:version => 20190307191045) do
     t.datetime "checked_comment_publication_at"
     t.string   "salt"
     t.string   "iv"
-    t.binary   "encrypted_comment_data"
+    t.binary   "encrypted_comment_data",           limit: 65535
     t.string   "agency_name"
     t.boolean  "agency_participating"
     t.string   "comment_document_number"
     t.string   "submission_key"
+    t.index ["agency_participating"], name: "index_comments_on_agency_participating", using: :btree
+    t.index ["comment_publication_notification"], name: "index_comments_on_comment_publication_notification", using: :btree
+    t.index ["comment_tracking_number"], name: "index_comments_on_comment_tracking_number", using: :btree
+    t.index ["created_at"], name: "index_comments_on_created_at", using: :btree
+    t.index ["document_number"], name: "index_comments_on_document_number", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
-  add_index "comments", ["agency_participating"], :name => "index_comments_on_agency_participating"
-  add_index "comments", ["comment_publication_notification"], :name => "index_comments_on_comment_publication_notification"
-  add_index "comments", ["comment_tracking_number"], :name => "index_comments_on_comment_tracking_number"
-  add_index "comments", ["created_at"], :name => "index_comments_on_created_at"
-  add_index "comments", ["document_number"], :name => "index_comments_on_document_number"
-  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
-
-  create_table "entry_emails", :force => true do |t|
+  create_table "entry_emails", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "remote_ip"
     t.integer  "num_recipients"
     t.integer  "entry_id"
     t.string   "sender_hash"
     t.string   "document_number"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  create_table "folders", :force => true do |t|
+  create_table "folders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "slug"
     t.integer  "creator_id"
@@ -79,24 +76,23 @@ ActiveRecord::Schema.define(:version => 20190307191045) do
     t.datetime "updated_at"
   end
 
-  create_table "mailing_lists", :force => true do |t|
-    t.text     "parameters"
+  create_table "mailing_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "parameters", limit: 65535
     t.string   "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.string   "type"
     t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_mailing_lists_on_deleted_at", using: :btree
   end
 
-  add_index "mailing_lists", ["deleted_at"], :name => "index_mailing_lists_on_deleted_at"
-
-  create_table "subscriptions", :force => true do |t|
+  create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "mailing_list_id"
     t.string   "requesting_ip"
     t.string   "token"
     t.datetime "unsubscribed_at"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.datetime "last_delivered_at"
     t.integer  "delivery_count"
     t.date     "last_issue_delivered"
@@ -105,11 +101,10 @@ ActiveRecord::Schema.define(:version => 20190307191045) do
     t.integer  "comment_id"
     t.datetime "deleted_at"
     t.string   "last_documents_delivered_hash"
+    t.index ["comment_id"], name: "index_subscriptions_on_comment_id", using: :btree
+    t.index ["deleted_at"], name: "index_subscriptions_on_deleted_at", using: :btree
+    t.index ["mailing_list_id", "deleted_at"], name: "index_subscriptions_on_mailing_list_id_and_deleted_at", using: :btree
+    t.index ["mailing_list_id", "last_documents_delivered_hash"], name: "mailing_list_documents", using: :btree
   end
-
-  add_index "subscriptions", ["comment_id"], :name => "index_subscriptions_on_comment_id"
-  add_index "subscriptions", ["deleted_at"], :name => "index_subscriptions_on_deleted_at"
-  add_index "subscriptions", ["mailing_list_id", "deleted_at"], :name => "index_subscriptions_on_mailing_list_id_and_deleted_at"
-  add_index "subscriptions", ["mailing_list_id", "last_documents_delivered_hash"], :name => "mailing_list_documents"
 
 end
