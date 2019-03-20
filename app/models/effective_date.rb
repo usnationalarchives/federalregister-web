a7865565da642466cf3233c3ba74e62a5b1b157b
Brandon Carag <brandon@criticaljuncture.org>
2019-03-19 17:44:08 -0700

    Fix effective date API url

--- app/models/effective_date.rb
+++ app/models/effective_date.rb
@@ -1,16 +1,14 @@
 class EffectiveDate
 
   def self.find_by_start_date_and_end_date(start_date, end_date)
-    response = HTTParty.get(url, query: {start_date: start_date, end_date: end_date})
+    response = HTTParty.get(url, query: {start_date: start_date.to_s(:iso), end_date: end_date.to_s(:iso)})
     response.parsed_response if response.code == 200
   end
 
   private
 
   def self.url
-    return 'http://www.mocky.io/v2/5c9033263600005500f10056' #TODO: Remove stub
-
-    "#{Settings.federal_register.api_url}/effective_dates"
+    "#{Settings.federal_register.api_url}/effective-dates"
   end
 
 end
