5312ce8ba33b59b5c935f0e55d05bc1e6357c1d1
Peregrinator <bob.burbach@gmail.com>
2020-02-28 15:34:31 -0800

    Update: Change loop syntax to be more readable. Use s3api to ensure the log is put in the 'folder' structure we desire. Fix empty file glob issues.

--- docker/web/files/logrotate/persist_logs.sh
+++ docker/web/files/logrotate/persist_logs.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 set -eu
 
+# don't return anything in file glob if no match
+shopt -s nullglob
+
 persist_logs=${PERSIST_LOGS:-false}
 k8s=${K8S:-false}
 
@@ -14,15 +17,15 @@ if [ "$persist_logs" = "true" ]; then
     pod_name=$(hostname)
   fi
 
-  logs=(/home/app/log/*.gz)
-  for ((i=0; i<${#logs[@]}; i++)); do
-    log_name=$(echo "${logs[$i]}" | rev | cut -d '/' -f1 | rev)
-    year=$(echo "${logs[$i]}" | cut -d '-' -f2 | cut -c1-4)
-    month=$(echo "${logs[$i]}" | cut -d '-' -f2 | cut -c5-6)
+  for f in /home/app/log/*.gz
+  do
+    log_name=$(echo "${f}" | rev | cut -d '/' -f1 | rev)
+    year=$(echo "${f}" | cut -d '-' -f2 | cut -c1-4)
+    month=$(echo "${f}" | cut -d '-' -f2 | cut -c5-6)
     upload_path="${year}/${month}/${pod_name}/${hostname}-${log_name}"
     
     if [ ! "$("aws s3 ls s3://${log_bucket}/${upload_path}")" ]; then
-      result=$("aws s3 cp ${logs[$i]} s3://${log_bucket}/${upload_path}")
+      result=$("aws s3api put-object --bucket ${log_bucket} --key ${upload_path} --body ${f}")
       echo "${result}"
     else
       echo "file ${upload_path} already exists"
