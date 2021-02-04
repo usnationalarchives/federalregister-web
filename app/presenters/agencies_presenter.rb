d6bf34ce20c4a690de7260c1e1dfd4c3b6e64c13
Brandon Carag <brandon@criticaljuncture.org>
2021-02-03 16:58:59 -0800

    Update: Suppress agencies with no documents from agency page.
    Use facet search to build a list of agencies with documents since it doesn’t return agencies without documents.
    
    Resolves #289.

--- app/presenters/agencies_presenter.rb
+++ app/presenters/agencies_presenter.rb
@@ -1,12 +1,13 @@
 class AgenciesPresenter
   attr_reader :agencies, :child_agencies
+  extend Memoist
 
   delegate :count, to: :@agencies
 
   def initialize
     @agencies = FederalRegister::Agency.all(
-      fields: ['id', 'name', 'parent_id', 'url']
-    )
+      fields: ['id', 'name', 'parent_id', 'url', 'slug']
+    ).select{|agency| agencies_with_documents[agency.slug] }
     @child_agencies = {}
   end
 
@@ -21,4 +22,13 @@ class AgenciesPresenter
     @child_agencies[parent_agency.id]= agencies.
       select{|agency| agency.parent_id == parent_agency.id}
   end
+
+  private
+
+  def agencies_with_documents
+    FederalRegister::Facet::Agency.search.each_with_object(Hash.new) do |facet, hsh|
+      hsh[facet.slug] = true
+    end
+  end
+  memoize :agencies_with_documents
 end
