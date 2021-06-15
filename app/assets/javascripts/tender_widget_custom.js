67306066bfed168cc5ce2483c4ebd10b9107017a
Brandon Carag <brandon@criticaljuncture.org>
2021-06-15 12:34:29 -0700

    Remove: Legacy code associated with rendering Tender.

--- app/assets/javascripts/tender_widget_custom.js
+++ app/assets/javascripts/tender_widget_custom.js
@@ -1,16 +1,5 @@
 (function(){
 
-  var getHost = function(){
-    var scripts = document.getElementsByTagName("script");
-    for(var i=0; i<scripts.length; i++){
-      var script = scripts[i].src;
-      if(script.match(/tender_widget\.js(\?.*)?$/)){
-        var host = "https://federalregister.tenderapp.com/";  //script.substring(0, script.indexOf("tender_widget.js"));
-      }
-    }
-    return host;
-  };
-
   var articlePageRegEx = new RegExp('\/documents\/\\d{4}');
   var articlePage = function() {
     return articlePageRegEx.test(window.location.href);
@@ -79,8 +68,6 @@
 
   var visible = false;
   var initialized = false;
-  var host = getHost();
-  if (!host || host === "") host = "https://federalregister.tenderapp.com/";
 
   var showWidget = function(){
     if( articlePage() ) {
@@ -92,7 +79,6 @@
 
   var show = function(){
     if (!initialized) {
-      // initialize();
       initializeZenDesk();
     } else {
       document.getElementById('tender_window').style.display = '';
@@ -110,29 +96,6 @@
     new FR2.ZendeskFormHandler
   }
 
-  var initialize = function(){
-    var element = document.createElement('div');
-    var url     = host + 'widget/discussion/new?r=' + Math.random() + '&discussion[body]=' + encodeURIComponent( "\n\n\n\n\n-----------------\n" + "URL: " + window.location + "\n" + "BROWSER: " + navigator.userAgent) ;
-    if (typeof(Tender) !== "undefined" && Tender.sso)
-      url += "&sso=" + encodeURIComponent(Tender.sso)
-    if(typeof Tender !== 'undefined' && Tender.widgetEmail)
-      url += '&email=' + encodeURIComponent(Tender.widgetEmail)
-
-    var wrapper = '<div id="tender_window"><a href="#" id="tender_closer">Close</a><div id="tender_frame"><iframe src="' + url + '" scrolling="no" frameborder="0" width="100%" height="100%"></iframe></div></div>';
-    element.innerHTML = wrapper;
-    var iframe = element.getElementsByTagName('iframe')[0];
-    document.body.appendChild(element);
-    var close_link = document.getElementById('tender_closer');
-    close_link.onclick = function(){
-      $('.jqmOverlay').remove();
-      hide();
-      return false;
-    };
-
-    initialized = true;
-    show();
-  };
-
   if (typeof(Tender) != "undefined" && Tender.widgetToggles){
     for(var i=0; i<Tender.widgetToggles.length; i++){
       var toggle = Tender.widgetToggles[i];
@@ -142,26 +105,6 @@
         return false;
       };
     }
-  }
-
-  var styles = "#tender_window{ position:absolute; top:20px; left:50%; margin-left:-340px; width:680px; height:615px; padding:3px; background:url(" + host + "images/widget/overlay_back.png); z-index:9999; }";
-  styles +=    "#tender_window iframe{ border:none; width:100%; height:100%; } ";
-  styles +=    "#tender_window #tender_frame{ width:100%; height:100%; background:url(" + host + "images/widget/loader.gif) 50% 50% no-repeat #fff; } ";
-  styles +=    "#tender_closer{ position:absolute; top:18px; right:18px; color:#fff; font-family:Helvetica, Arial, sans-serif; font-size:12px; font-weight:bold; text-decoration:none; border:none; } ";
-  styles +=    "#tender_toggler{ position:absolute; top:100px; right:0px; width:33px; height:105px; padding:3px 0 3px 3px; background:url(" + host + "images/widget/overlay_back.png); } ";
-  styles +=    "#tender_toggler_link{ display:block; width:100%; height:100%; text-decoration:none; border:none; background:#006699; text-indent:-9999px; background:url(" + host + "images/widget/tab_text.gif); } ";
-
-  var style = document.createElement('style');
-  style.setAttribute("type", "text/css");
-  style.setAttribute("charset", "utf-8");
-  try{ // For safari's sake
-    style.appendChild(document.createTextNode(styles));
-    document.getElementsByTagName("head").item(0).appendChild(style);
-  }catch(e){ }
-
-  // For IE
-  if(document.createStyleSheet) {
-    document.createStyleSheet(host + 'tender_widget_styles.css');
   }
 
 })();
