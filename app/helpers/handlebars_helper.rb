2d920672f212dc8c23a38b7a24b2ff1a169b2e8a
Brandon Carag <brandon@criticaljuncture.org>
2022-01-14 16:44:01 -0800

    Fix: Handlebar template inclusion deprecation.
    Simply calling `render template` instead of `render file` here without doing a lookup context-based call causes a missing template error.
    
    By using the custom #locate_template method below, we're mimicking the fallback template logic Rails 6.0 uses.  See Rails source here: https://github.com/rails/rails/blob/6-0-stable/actionview/lib/action_view/renderer/template_renderer.rb#L28-L32
    
    See the Rails commit below for broader context around the intent around render file vs render template:
    
    https://github.com/rails/rails/commit/c7820d8124c854760a4d288334f185de2fb99446

--- app/helpers/handlebars_helper.rb
+++ app/helpers/handlebars_helper.rb
@@ -5,8 +5,18 @@ module HandlebarsHelper
     html = ['<script id="']
     html << id
     html << '-template" type="text/x-handlebars-template">'
-    html << render(file: Rails.root.join('app', 'templates', "#{name}.handlebars"))
+    path = Rails.root.join('app', 'templates', "#{name}.handlebars") 
+    #NOTE: Simply calling `render template` here without doing a lookup context-based call causes a missing template error.  By using the custom #locate_template method below, we're mimicking the fallback template logic Rails 6.0 uses.  See Rails source here: https://github.com/rails/rails/blob/6-0-stable/actionview/lib/action_view/renderer/template_renderer.rb#L28-L32
+    html << render(template: locate_template(path))
     html << '</script>'
     html.join('').html_safe
   end
+
+  private
+
+  def locate_template(name)
+    lookup_context = ActionView::LookupContext.new(ActionController::Base.view_paths)
+    lookup_context.with_fallbacks.find_template(name)
+  end
+
 end
