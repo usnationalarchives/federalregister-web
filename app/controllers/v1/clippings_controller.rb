ebdbe6e806a508c67119007bdc9d49a33a5042b5
Peregrinator <bob.burbach@gmail.com>
2023-08-22 09:39:47 -0700

    Fix for users with no clippings

--- app/controllers/v1/clippings_controller.rb
+++ app/controllers/v1/clippings_controller.rb
@@ -9,10 +9,10 @@ class V1::ClippingsController < ApiController
         includes(:folder).
         with_preloaded_documents
 
-      @folders = @clippings.map(&:folder).uniq
+      @folders = @clippings.nil? ? [] : @clippings.map(&:folder).uniq
 
       # roll up clippings not in a folder into the clipboard
-      clipboard_clippings = @clippings.select{|c| c.folder_id.nil?}
+      clipboard_clippings = @clippings&.select{|c| c.folder_id.nil?} || []
       clipboard.doc_count = clipboard_clippings.count
       clipboard.documents = clipboard_clippings.map(&:document_number)
       clipboard.document_types = clipboard_clippings.map{|c| c.document.type.downcase.gsub(" ", "_")}.uniq.compact
