f95330bd6b69fe9084a60c0a14eb60607d3a6afb
Brandon Carag <brandon@criticaljuncture.org>
2023-10-02 12:56:47 -0700

    Update: Verbiage around official notice

--- app/helpers/public_inspection_helper.rb
+++ app/helpers/public_inspection_helper.rb
@@ -8,7 +8,7 @@ module PublicInspectionHelper
     "If you are using public inspection listings for legal research, you
     should verify the contents of the documents against a final, official
     edition of the Federal Register. <strong>Only official editions of the
-    Federal Register provide legal notice to the public and judicial notice
+    Federal Register provide legal notice of publication to the public and judicial notice
     to the courts under #{public_inspection_uscode_link}.</strong>
     Learn more #{link_to 'here', public_inspection_learn_path}.".html_safe
   end
