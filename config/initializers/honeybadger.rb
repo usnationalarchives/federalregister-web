cbcfd61e8e517e77174b5a55ad317f08574ec9b3
Peregrinator <bob.burbach@gmail.com>
2024-02-16 14:38:29 -0800

    Add container role to settings - update honeybadger context

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,3 +1,14 @@
 Honeybadger.configure do |config|
   config.api_key = Rails.application.credentials.dig(:honeybadger, :api_key)
+
+  config.revision = Settings.container.revision
+
+  config.before_notify << lambda do |notice|
+    notice.context.merge!(
+      application: "web",
+      deployment_environment: Settings.container.role,
+      tags: "#{Settings.container.process}_process, #{Settings.container.role}",
+      revision: Settings.container.revision
+    )
+  end
 end
