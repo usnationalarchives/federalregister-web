486b0661764b5f70f12c52cdc81bb9be3a361ba2
Peregrinator <bob.burbach@gmail.com>
2024-03-04 15:42:51 -0800

    Remove dead code

--- app/assets/javascripts/storage.js
+++ app/assets/javascripts/storage.js
@@ -9,24 +9,6 @@ function expect_logged_in() {
   }
 }
 
-function show_folder_success(response) {
-  var new_p = $('<p>').append(
-    'Successfully created folder "' + response.folder.name + '"'
-  ).append(
-    $('<span>').addClass('icon-fr2 icon-fr2-badge_check_mark')
-  );
-  $('#new-folder-modal .folder_success p').replaceWith( new_p );
-  $('#new-folder-modal .folder_success').show();
-}
-
-function set_clipped_icon_status() {
-  if ( $('#add-to-folder .menu li.in_folder').length > 0 ) {
-    $('#add-to-folder .button .icon').addClass('clipped');
-  } else {
-    $('#add-to-folder .button .icon').removeClass('clipped');
-  }
-}
-
 function closeOnEscape(hash) {
   $(window).one('keyup', function(event) {
     if( event.keyCode === '27' ){
