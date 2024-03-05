e74eaa01a86108e78edb7d0bd52d1958db32b72c
Peregrinator <bob.burbach@gmail.com>
2024-03-04 16:14:10 -0800

    Remove unused js cookie setting

--- app/assets/javascripts/old/application.js
+++ app/assets/javascripts/old/application.js
@@ -1,6 +1,3 @@
 $(document).ready(function () {
-    // let the server know the user has JS enabled
-    document.cookie = "javascript_enabled=1; path=/";
-
     $("input[placeholder]").textPlaceholder();
 });
