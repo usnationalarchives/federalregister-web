ab77d4834d15c87010214594b3fe235a9c77db98
Peregrinator <bob.burbach@gmail.com>
2024-04-10 10:27:39 -0700

    Move title font size code into shared decorator

--- app/decorators/document_decorator/shared.rb
+++ app/decorators/document_decorator/shared.rb
@@ -24,4 +24,18 @@ module DocumentDecorator::Shared
   def shortened_abstract(length=500)
     h.truncate_words(model.abstract, :length => length) || "<em>[Not available]</em>".html_safe
   end
+
+    # we mark long titles so that we can reduce their font size
+    def title_class
+      case title.size
+      when 0..115
+        ""
+      when 116..300
+        "large-title"
+      when 301..600
+        "extra-large-title"
+      else
+        "jumbo-title"
+      end
+    end
 end
