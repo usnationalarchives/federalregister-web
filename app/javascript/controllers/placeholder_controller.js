f7b70cb99448e5ca5f5b4c35aa9da8419848e88c
Brandon Carag <brandon@criticaljuncture.org>
2024-09-20 11:55:46 -0700

    Fix: Add support for dynamically selecting a different omni search modal depending on the search box clicked
    e.g. on agency pages, we want users to be able to click in to the agency-specific search and for the global nav search to be the global search

--- app/javascript/controllers/placeholder_controller.js
+++ app/javascript/controllers/placeholder_controller.js
@@ -3,6 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   suggestions(event) {
     event.preventDefault();
-    Stimulus.getControllerForElementAndIdentifier(document.querySelector('#suggestions'), "suggestions").modal();
+
+    if (this.data.get('agency-scoped-search')) {
+      var querySelector = '#suggestions.agency-scoped-search'
+    } else {
+      var querySelector = '#suggestions'
+    }
+
+    Stimulus.getControllerForElementAndIdentifier(document.querySelector(querySelector), "suggestions").modal();
   }
 }
