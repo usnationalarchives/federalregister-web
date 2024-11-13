e753aa03d2f93bbc20bbdc142ac39bc57145a33d
Brandon Carag <brandon@criticaljuncture.org>
2024-11-13 16:52:37 +0000

    Fix: Faraday API change for basic auth

--- lib/ofr/user_email_result_set.rb
+++ lib/ofr/user_email_result_set.rb
@@ -1,11 +1,13 @@
 class Ofr::UserEmailResultSet
   def self.get_user_emails(user_ids)
-    conn = Faraday.new
-    conn.basic_auth(
-      Rails.application.credentials.dig(:services, :ofr, :profile, :basic_username),
-      Rails.application.credentials.dig(:services, :ofr, :profile, :basic_password)
-    )
-    response = conn.post "#{Settings.services.ofr.profile.internal_base_url}/api/profile/v1/emails", {ids: Array.wrap(user_ids)}
+    conn = Faraday.new do |conn|
+      conn.request :json            # Encode request bodies as JSON
+      conn.request :authorization,
+        :basic,
+        Rails.application.credentials.dig(:services, :ofr, :profile, :basic_username),
+        Rails.application.credentials.dig(:services, :ofr, :profile, :basic_password)
+    end
+    response = conn.post "#{Settings.services.ofr.profile.internal_base_url}/api/profile/v1/emails", {ids: user_ids}
     JSON.parse(response.body)
   end
 end
